81d455e21876653a7054f5f7b04d8e301ab23868
Daniel Aguilar <dacmail@gmail.com>
2018-06-05 18:26:17 +0200

    Navbar component

---
+++ src/components/navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar navbar-default"><div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/"><img src="img/tipi-ods.svg"></a>
+      </div>
+      <div id="main-menu" class="collapse navbar-collapse" aria-expanded="false">
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <router-link to="/" class="menuitem">Search</router-link>
+          </li>
+          <li>
+            <router-link to="/about" class="menuitem">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'navbar'
+}
+</script>
