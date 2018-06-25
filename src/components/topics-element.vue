fcea3d0c1588ef2bcbca26fffd72171c9bc95dd7
pr3ssh <x@pr3ssh.net>
2018-06-25 13:47:28 +0200

    Add subtopics and tags to initiative view

--- src/components/topics-element.vue
+++ src/components/topics-element.vue
@@ -5,14 +5,14 @@
   <ul class="list-unstyled">
     <li v-for="topic in topics" v-bind:key="topic" class="value">
       <router-link :to="{ name: 'results', params: { topic: topic } }">{{topic}}</router-link>
-    <!--   <br> -->
-    <!--   <small> -->
-    <!--     (Por mencionar: -->
-    <!--     {{#each getTerms ../terms this}} -->
-    <!--     <a href="/escaner?dicts={{..}}&#38;terms[]={{this}}">{{this}}</a>,&#38;nbsp; -->
-    <!--     {{/each}} -->
-    <!--     ) -->
-    <!--   </small> -->
+      <br>
+      <div v-for="subtopic in getSubtopics(topic)" v-bind:key="subtopic" class="subtopic">
+        {{subtopic}}
+        <br>
+        <span v-for="tag in getTags(subtopic)" v-bind:key="tag" class="tag">{{tag}}</span>
+        <br>
+      </div>
+
     </li>
   </ul>
   </p>
@@ -23,7 +23,33 @@ export default {
   name: 'topics-element',
   props: [
     'meta',
-    'topics'
-  ]
+    'topics',
+    'tags'
+  ],
+  methods: {
+    getSubtopics(topic) {
+      return [...new Set(this.$props.tags.filter(tag => tag.topic === topic).map(tag => tag.subtopic))];
+    },
+    getTags: function(subtopic) {
+      return this.$props.tags.filter(tag => tag.subtopic === subtopic).map(tag => tag.tag)
+    },
+  }
 }
 </script>
+
+<style scoped lang="scss">
+.value {
+  .subtopic {
+    font-size: 0.8em;
+    margin-left: 20px;
+    .tag {
+      background-color: lighten(#abe8ff, 5%);
+      color: #222; 
+      padding: 5px;
+      margin: 0px 5px 5px 5px;
+      font-size: 0.8em;
+      font-weight: normal;
+    }
+  }
+}
+</style>
