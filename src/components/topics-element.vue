8ba953d9e86fa8a47d6f60f56d01157f20ef5eed
pr3ssh <x@pr3ssh.net>
2018-06-25 09:42:25 +0200

    Created topics-element component and added to initiative view

---
+++ src/components/topics-element.vue
@@ -0,0 +1,29 @@
+<template>
+  <p v-if="topics">
+  <span class="meta">{{meta}}</span>
+  <br>
+  <ul class="list-unstyled">
+    <li v-for="topic in topics" class="value">
+      {{topic}}
+    <!--   <br> -->
+    <!--   <small> -->
+    <!--     (Por mencionar: -->
+    <!--     {{#each getTerms ../terms this}} -->
+    <!--     <a href="/escaner?dicts={{..}}&#38;terms[]={{this}}">{{this}}</a>,&#38;nbsp; -->
+    <!--     {{/each}} -->
+    <!--     ) -->
+    <!--   </small> -->
+    </li>
+  </ul>
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'topics-element',
+  props: [
+    'meta',
+    'topics'
+  ]
+}
+</script>
