40a0c44031db1e06eee352345810e304a3235158
pr3ssh <x@pr3ssh.net>
2018-06-26 19:48:09 +0200

    Change parliamentary group endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -18,7 +18,7 @@ export default {
     function getEndpoint() {
       return [
         config.URL,
-        '/parliamentarygroups/'
+        '/parliamentary-groups/'
       ].join('');
     }
   },
