0c5de09acc2215c6d89a6280eba9ac0b6aae7a63
pr3ssh <x@pr3ssh.net>
2018-06-28 18:38:29 +0200

    Add header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
+    <div style="width:100%;height:10px;background: url('/img/cascade.png') repeat-x;"></div>
     <navbar></navbar>
     <router-view/>
+    <div class="container-fluid">
+      <div id="credits" class="row">
+        <div class="col-sm-12 text-center">
+          <small><strong>TIPI</strong> está licenciado bajo <a href="https://www.gnu.org/copyleft/gpl.html" target="_blank">GNU GPL v3</a></small>
+          <br>
+          <small>Hecho con ♥ por <a href="https://unmundosalvadorsoler.org/ciecode/" target="_blank">CIECODE (Fundacion Salvador Soler)</a></small>
+        </div>
+      </div>
+    </div>
+    <div style="width:100%;height:10px;background: url('/img/cascade.png') repeat-x;"></div>
   </div>
 </template>
 
