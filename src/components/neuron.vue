be5f8590f380474bdb67436993bc33f34261cc08
pr3ssh <x@pr3ssh.net>
2018-07-04 16:52:25 +0200

    Add watch to .initiative on neuron component

--- src/components/neuron.vue
+++ src/components/neuron.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg style="width:100%; height:600px;"></svg>
+  <svg style="width:100%; height:450px;"></svg>
 </template>
 
 <script>
@@ -9,183 +9,190 @@ const d3 = require('d3');
 export default {
   name: 'neuron',
   props: ['initiative'],
-  mounted: function() {
-    var self = this;
+  methods: {
+    loadVizz: function() {
+      var self = this;
 
-    this.initiative = this.$props.initiative;
+      console.log("loading vizz");
 
-    api.getTopics()
-      .then(topics => {
-        this.topics = topics;
-        //TODO:  We should use the ID of the initiative on the SVG element
-        //       so that we can have multiple viz in the same page, not rely on querystrinty
-        //       and make the creation of the viz on the HTML side completely transparent.
-        svg = d3.select(this.$el);
-        width = +svg.node().getBoundingClientRect().width;
-        height = +svg.node().getBoundingClientRect().height;
-        onDataReady();
-      })
-      .catch(error => this.errors = error);
+      this.initiative = this.$props.initiative;
 
-
-
-    //=== Utilities ===
-    //Mapping ranges
-    function map(x, in_min, in_max, out_min, out_max) {
-        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    }
-
-    //Converting polar coordinates to cartesian coordinates
-    function polar(angle, radius) {
-      return [
-      Math.cos(angle)*radius,
-      Math.sin(angle)*radius
-      ]
-    }
-
-    //Calculate the transform of a node
-    function calculateTransform(d,i) {
-        var pos = calculatePosition(d,i);
-        return "translate(" + pos[0] + "," + pos[1] + ")";
-    }
-
-    //Calculate the position of a node
-    function calculatePosition(d, i) {
-        var angle = map(i, 0, data.length, 0, Math.PI*2);
-        var pos = polar(angle, d["related"] ? 150 : 100);
-        return [pos[0] + width/2, pos[1] + height/2];
-    }
+      api.getTopics()
+        .then(topics => {
+          this.topics = topics;
+          //TODO:  We should use the ID of the initiative on the SVG element
+          //       so that we can have multiple viz in the same page, not rely on querystrinty
+          //       and make the creation of the viz on the HTML side completely transparent.
+          svg = d3.select(this.$el);
+          width = +svg.node().getBoundingClientRect().width;
+          height = +svg.node().getBoundingClientRect().height;
+          onDataReady();
+        })
+        .catch(error => this.errors = error);
 
 
-    //=== Data visualization ===
 
-    //Configurations
-    var color = {
-      "related": {
-        "ODS 6": "#00aed9",
-        "ODS 7": "#fdb713",
-        "ODS 11": "#f99d26",
-        "ODS 12": "#cf8d2a",
-        "ODS 15": "#3eb049",
-      },
-      "notRelated": "#cecece",
-      "center": "#888",
-      "line": "#cecece"
-    }
-
-    //TODO: Encapsulate this in a class so we can have multiple vizualisations in one page.
-    //Globals
-    var data = [];
-    var svg;
-    var width;
-    var height;
-
-
-    function onDataReady() {
-
-      for(var d=0; d<self.topics.length; d++) {
-        var newNode = {"name": self.topics[d].name, "related": false, "intensity": 0};
-        for(var t=0; t<self.initiative["topics"].length; t++) {
-          if(self.initiative["topics"][t] == newNode["name"]) {
-            newNode["related"] = true;
-          }
-        }
-
-        for(var i=0; i<self.initiative["tags"].length; i++) {
-          if(self.initiative["tags"][i]["topic"] == newNode["name"]) {
-            newNode["intensity"]++;
-          }
-        }
-        //intensity goes from 0 to 1 (percentage of tags)
-        newNode["intensity"] = newNode["intensity"]/self.initiative["tags"].length;
-
-        data.push(newNode);
+      //=== Utilities ===
+      //Mapping ranges
+      function map(x, in_min, in_max, out_min, out_max) {
+          return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
       }
 
-      console.log("Data ready");
+      //Converting polar coordinates to cartesian coordinates
+      function polar(angle, radius) {
+        return [
+        Math.cos(angle)*radius,
+        Math.sin(angle)*radius
+        ]
+      }
 
-      createNodes();
-    }
+      //Calculate the transform of a node
+      function calculateTransform(d,i) {
+          var pos = calculatePosition(d,i);
+          return "translate(" + pos[0] + "," + pos[1] + ")";
+      }
 
-    function createNodes() {
-      svg.selectAll(".connection")
+      //Calculate the position of a node
+      function calculatePosition(d, i) {
+          var angle = map(i, 0, data.length, 0, Math.PI*2);
+          var pos = polar(angle, d["related"] ? 150 : 100);
+          return [pos[0] + width/2, pos[1] + height/2];
+      }
+
+
+      //=== Data visualization ===
+
+      //Configurations
+      var color = {
+        "related": {
+          "ODS 6": "#00aed9",
+          "ODS 7": "#fdb713",
+          "ODS 11": "#f99d26",
+          "ODS 12": "#cf8d2a",
+          "ODS 15": "#3eb049",
+        },
+        "notRelated": "#cecece",
+        "center": "#888",
+        "line": "#cecece"
+      }
+
+      //TODO: Encapsulate this in a class so we can have multiple vizualisations in one page.
+      //Globals
+      var data = [];
+      var svg;
+      var width;
+      var height;
+
+
+      function onDataReady() {
+
+        for(var d=0; d<self.topics.length; d++) {
+          var newNode = {"name": self.topics[d].name, "related": false, "intensity": 0};
+          for(var t=0; t<self.initiative["topics"].length; t++) {
+            if(self.initiative["topics"][t] == newNode["name"]) {
+              newNode["related"] = true;
+            }
+          }
+
+          for(var i=0; i<self.initiative["tags"].length; i++) {
+            if(self.initiative["tags"][i]["topic"] == newNode["name"]) {
+              newNode["intensity"]++;
+            }
+          }
+          //intensity goes from 0 to 1 (percentage of tags)
+          newNode["intensity"] = newNode["intensity"]/self.initiative["tags"].length;
+
+          data.push(newNode);
+        }
+
+        console.log("Data ready");
+
+        createNodes();
+      }
+
+      function createNodes() {
+        svg.selectAll(".connection")
+          .data(data)
+          .enter().append("path").transition()
+            .duration(700)
+            .delay(function(d, i) { return i * i + 500; })
+            .ease(d3.easeBackOut)
+            .attr("d", function(d,i){
+                var pos = calculatePosition(d,i);
+                return "M " + width/2 + " " + height/2
+                     + "L " + pos[0] + " " + pos[1];
+              })
+            .attr("stroke-width",function(d){return d["related"] ? '3px' : '1px'})
+            .attr("stroke", function(d){ return d["related"] ? color["related"][d["name"].split('-')[0].trim()] : color["line"]});
+
+        var node = svg.selectAll(".node")
         .data(data)
-        .enter().append("path").transition()
-          .duration(700)
-          .delay(function(d, i) { return i * i + 500; })
-          .ease(d3.easeBackOut)
-          .attr("d", function(d,i){
-              var pos = calculatePosition(d,i);
-              return "M " + width/2 + " " + height/2
-                   + "L " + pos[0] + " " + pos[1];
-            })
-          .attr("stroke-width",function(d){return d["related"] ? '3px' : '1px'})
-          .attr("stroke", function(d){ return d["related"] ? color["related"][d["name"].split('-')[0].trim()] : color["line"]});
+        .enter().append("g")
+          .attr("class", "node")
+          .attr("transform", function(){return "translate(" +  width/2 + "," + height/2 + ")";});
 
-      var node = svg.selectAll(".node")
-      .data(data)
-      .enter().append("g")
-        .attr("class", "node")
-        .attr("transform", function(){return "translate(" +  width/2 + "," + height/2 + ")";});
-
-      node.transition()
-      .duration(700)
-      .delay(function(d, i) { return i * i; })
-      .ease(d3.easeBackOut)
-      .attr("transform", calculateTransform);
-
-      node.append("circle")
-          .attr("class", function(d){return d["related"] ? "related" : ""})
-          .attr("r", function(d){return d["related"] ? 15 : 10})
-          .style("fill", function(d){ return d["related"] ? color["related"][d["name"].split('-')[0].trim()] : color["notRelated"]});
-
-      node.append("path")
-          .attr("d", d3.arc()
-            .innerRadius(function (d){return d["related"] ? 20 : 15})
-            .outerRadius(function (d){return d["related"] ? 25 : 20})
-            .startAngle(0)
-            .endAngle(function (d){
-              return Math.PI * 2 * d["intensity"]}))
-          .attr("class", function(d){return d["related"] ? "related" : ""})
-          .style("fill", function(d){ return d["related"] ? color["related"][d["name"].split('-')[0].trim()] : color["notRelated"]});
-
-      node.append("text")
-          .attr("class", "label_name")
-          .attr("text-anchor", "middle")
-          .attr("alignment-baseline", "central")
-          .text(function(d) { return d["related"] ? d["name"].split('-')[0].trim() : ""; });
-          //.text(function(d){return d["related"] ? Math.round(d["intensity"]*100) + "%" : ""})
-
-       /*
-      node.append("text")
-          .attr("class", "label_name")
-          .attr("transform", function(d,i) {
-            var angle = map(i, 0, data.length, 0, Math.PI*2);
-            var flip = 0;
-            if (angle > Math.PI/2 && angle < 3*Math.PI/2) {
-              flip = 180;
-            }
-            var size = 0;
-            if(flip >0) {
-              size = d["name"].length*7;
-            }
-            return "translate(" + (Math.cos(angle)*(25 + size)) + "," + (Math.sin(angle)*(25+size)) + "), rotate("+ ((angle)*(180/Math.PI) + flip) + ")";
-          })
-          .text(function(d) { return d["related"] ? d["name"].split('-')[0].trim() : ""; });
-      */
-
-      var center = svg.append("g")
-        .attr("class", "center")
-        .attr("transform", "translate("+width/2+", "+height/2+")")
-
-      center.append("circle")
-        .attr("r", 5)
-        .attr("fill", color["center"])
-        .transition()
+        node.transition()
         .duration(700)
+        .delay(function(d, i) { return i * i; })
         .ease(d3.easeBackOut)
-        .attr("r", 20);
+        .attr("transform", calculateTransform);
+
+        node.append("circle")
+            .attr("class", function(d){return d["related"] ? "related" : ""})
+            .attr("r", function(d){return d["related"] ? 15 : 10})
+            .style("fill", function(d){ return d["related"] ? color["related"][d["name"].split('-')[0].trim()] : color["notRelated"]});
+
+        node.append("path")
+            .attr("d", d3.arc()
+              .innerRadius(function (d){return d["related"] ? 20 : 15})
+              .outerRadius(function (d){return d["related"] ? 25 : 20})
+              .startAngle(0)
+              .endAngle(function (d){
+                return Math.PI * 2 * d["intensity"]}))
+            .attr("class", function(d){return d["related"] ? "related" : ""})
+            .style("fill", function(d){ return d["related"] ? color["related"][d["name"].split('-')[0].trim()] : color["notRelated"]});
+
+        node.append("text")
+            .attr("class", "label_name")
+            .attr("text-anchor", "middle")
+            .attr("alignment-baseline", "central")
+            .text(function(d) { return d["related"] ? d["name"].split('-')[0].trim() : ""; });
+            //.text(function(d){return d["related"] ? Math.round(d["intensity"]*100) + "%" : ""})
+
+         /*
+        node.append("text")
+            .attr("class", "label_name")
+            .attr("transform", function(d,i) {
+              var angle = map(i, 0, data.length, 0, Math.PI*2);
+              var flip = 0;
+              if (angle > Math.PI/2 && angle < 3*Math.PI/2) {
+                flip = 180;
+              }
+              var size = 0;
+              if(flip >0) {
+                size = d["name"].length*7;
+              }
+              return "translate(" + (Math.cos(angle)*(25 + size)) + "," + (Math.sin(angle)*(25+size)) + "), rotate("+ ((angle)*(180/Math.PI) + flip) + ")";
+            })
+            .text(function(d) { return d["related"] ? d["name"].split('-')[0].trim() : ""; });
+        */
+
+        var center = svg.append("g")
+          .attr("class", "center")
+          .attr("transform", "translate("+width/2+", "+height/2+")")
+
+        center.append("circle")
+          .attr("r", 5)
+          .attr("fill", color["center"])
+          .transition()
+          .duration(700)
+          .ease(d3.easeBackOut)
+          .attr("r", 20);
+      }
     }
+  },
+  watch: {
+    '$props.initiative': 'loadVizz'
   }
 }
 </script>
