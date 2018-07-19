91ae3f3dc0d6bf87cf516b4421e533017d408624
pr3ssh <x@pr3ssh.net>
2018-07-19 10:07:39 +0200

    Reload mediakit

--- src/views/Mediakit.vue
+++ src/views/Mediakit.vue
@@ -8,14 +8,14 @@
           <div class="col-sm-6 language-block text-center">
               <h4>Versión en español</h4>
               <div class="download-block">
-                <a href="/Mediakit-ES.zip" class="btn btn-custom-naked">
+                <a href="/mediakit-es.zip" class="btn btn-custom-naked">
                   <i class="fa fa-download fa-2x"></i><br/>Descargar kit
                 </a>
               </div>
               <p>
                 Este kit incluye:
                 <ul class="list-unstyled">
-                  <li>Dossier</li>
+                  <li>Dossier (+ extendido)</li>
                   <li>Nota de prensa</li>
                   <li>Logos oficiales</li>
                 </ul>
@@ -24,14 +24,14 @@
           <div class="col-sm-6 language-block text-center">
               <h4>English version</h4>
               <div class="download-block">
-                <a href="/Mediakit-EN.zip" class="btn btn-custom-naked">
+                <a href="/mediakit-en.zip" class="btn btn-custom-naked">
                   <i class="fa fa-download fa-2x"></i><br/>Download kit
                 </a>
               </div>
               <p>
                 This kit includes:
                 <ul class="list-unstyled">
-                  <li>Dossier</li>
+                  <li>Dossier (+ extended)</li>
                   <li>Official logos</li>
                 </ul>
               </p>
