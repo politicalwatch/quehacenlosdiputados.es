23760eb61c9c4890d3344c93c94ed0fd7c70c0e2
Daniel Aguilar <dacmail@gmail.com>
2018-09-03 10:34:39 +0200

    Remove BS JS and jQuery files

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,6 @@
 let hamburger = document.querySelector('.navbar-toggle');
 let menu = document.querySelector(hamburger.dataset.target);
 
-console.log(menu);
-
 function collapseMenu(e) {
   e.preventDefault();
   menu.classList.toggle('in');
