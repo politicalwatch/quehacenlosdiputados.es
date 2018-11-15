56bdbaee399b093468bc1c66658fd6c75fa79717
pr3ssh <x@pr3ssh.net>
2018-11-15 09:17:30 +0100

    Check if the value is not undefined (text-element component)

--- src/components/text-element.vue
+++ src/components/text-element.vue
@@ -19,6 +19,7 @@ export default {
       return value
     },
     is: function(value) {
+      if (value === undefined) return false;
       if (value.constructor === String && value != "") return true;
       if (value.constructor === Array && value.length > 0) return true;
       return false;
