1389171cd891313d406e724920fefb82fc69d454
pr3ssh <x@pr3ssh.net>
2018-12-14 14:07:46 +0100

    Modify Splash screen

--- src/components/splash.vue
+++ src/components/splash.vue
@@ -3,8 +3,8 @@
     <div class="outer">
       <div class="inner">
         <img src="/img/logo.png">
-        <p><strong>Parlamento 2030</strong> te ofrece toda la información sobre la actividad del Congreso de los Diputados relacionada con los <strong>Objetivos de Desarrollo Sostenible</strong>. Entra y descubre cómo trabajan los diputados españoles de cara al cumplimiento de la <strong>Agenda 2030</strong>.</p>
-        <p>Estás a punto de entrar en la versión beta de Parlamento 2030. En ella encontrarás toda la información parlamentaria sobre los cinco ODS priorizados este año en el Foro Político de Alto Nivel de Naciones Unidas. Pronto estarán disponible los 17 ODS que configuran la Agenda 2030.</p>
+        <p><strong>Parlamento 2030</strong> te ofrece toda la información sobre la actividad del Congreso de los Diputados relacionada con los <strong>Objetivos de Desarrollo Sostenible (ODS)</strong>. Entra y descubre cómo trabajan los diputados españoles de cara al cumplimiento de la <strong>Agenda 2030</strong>.</p>
+        <p>Estás a punto de entrar en la versión beta de Parlamento 2030. En ella encontrarás toda la información parlamentaria relacionada con 6 de los 17 ODS que configuran la Agenda 2030. Estamos actualmente trabajando en el resto de ODS y pronto estarán disponible.</p>
         <p><a class="btn btn-custom btn-block close-splash" href="#" @click="closeSplash">Comenzar</a></p>
         <p>
           <router-link :to="{name: 'about-en'}">Learn more about this project in English</router-link>
