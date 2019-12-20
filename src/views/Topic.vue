24ba8666c0fb9ca1456852dee43523257418ec35
Daniel Aguilar <dacmail@gmail.com>
2019-12-20 11:16:23 +0100

    Topic layout

--- src/views/Topic.vue
+++ src/views/Topic.vue
@@ -1,70 +1,38 @@
 <template>
   <div>
-    <tipi-navbar :links="MENU" :disclaimerLink="DISCLAIMER" :logo="LOGO" />
     <tipi-header :title="topic.name"/>
     <div id="dict">
       <div class="container page">
-        <div class="row" v-if="topic.description.length > 0 && topic.icon != ''">
-          <div class="col-sm-3">
-            <img class="img-responsive" :src="'/img/topics/'+topic.icon" :alt="'Imagen de' + topic.name">
-          </div>
-          <div class="col-sm-9">
-            <p v-for="d in topic.description" v-bind:key="d" style="font-size:1.2em;">
-              {{d}}
-            </p>
-            <!-- <text&#45;element :meta="'¿De qué trata?'" :value="topic.description"></text&#45;element> -->
-          </div>
-        </div>
+        <tipi-topic-card :topic="topic" />
         <hr v-if="topic.description && topic.icon">
         <div class="row">
           <div class="col-sm-6" v-if="deputies">
-            <h4>Diputadas/os más activas/os</h4>
-            <ul class="list-unstyled">
-              <li v-for="deputy in deputies" v-bind:key="deputy._id">
-                <span class="foto mediana">
-                  <img class="img-responsive" :src="deputy.image" />
-                </span>
-                <span class="dipname">
-                  <router-link :to="{path: '/deputies/' + deputy.id}">{{deputy.name}}</router-link>
-                </span>
-              </li>
-            </ul>
+            <tipi-text meta="Diputadas/os más activas/os" :value="deputies" type="deputies" :source="deputies" />
           </div>
           <div class="col-sm-6" v-if="parliamentarygroups">
-            <h4>Grupos más activos</h4>
-            <ul class="list-unstyled">
-              <li v-for="parliamentarygroup in parliamentarygroups" v-bind:key="parliamentarygroup._id">
-                <span class="groupname"><router-link :to="{path: '/parliamentarygroups/' + parliamentarygroup.id}">{{parliamentarygroup.name}}</router-link></span>
-              </li>
-            </ul>
+            <tipi-text meta="Grupos más activoss" :value="parliamentarygroups" type="parliamentarygroups" :source="parliamentarygroups" />
           </div>
         </div>
         <hr v-if="latestInitiatives">
-        <latest-initiatives :initiatives="latestInitiatives"></latest-initiatives>
+        <h4 v-if="latestInitiatives">Últimas iniciativas</h4>
+        <tipi-results layout="tiny" :initiatives="latestInitiatives"/>
       </div>
     </div>
-    <footer-block></footer-block>
   </div>
 </template>
 
 <script>
 
-import { TipiHeader, TipiNavbar } from 'tipi-frontend-uikit'
-import FooterBlock from '@/components/footer-block';
-import LatestInitiatives from '@/components/latest-initiatives';
-import TextElement from '@/components/text-element';
+import { TipiHeader, TipiResults, TipiTopicCard, TipiText } from 'tipi-frontend-uikit'
 import api from '@/api';
-import config from '@/config';
-
 
 export default {
   name: 'topic',
   components: {
-    TipiNavbar,
     TipiHeader,
-    FooterBlock,
-    LatestInitiatives,
-    TextElement
+    TipiResults,
+    TipiTopicCard,
+    TipiText,
   },
   data: function() {
     return {
@@ -74,9 +42,6 @@ export default {
       allParliamentarygroups: null,
       parliamentarygroups: null,
       latestInitiatives: null,
-      MENU: config.MENU,
-      DISCLAIMER: config.DISCLAIMER,
-      LOGO: config.LOGO,
     }
   },
   methods: {
@@ -147,6 +112,3 @@ export default {
   }
 }
 </script>
-
-<style scoped lang="scss">
-</style>
