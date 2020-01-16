3a7b5fb74a7a2f3bda6dc7b7a3b480034b966be0
Daniel Aguilar <dacmail@gmail.com>
2020-01-16 13:19:52 +0100

    Install and use vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import Vuex from 'vuex'
 
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
 
 new Vue({
   router,
