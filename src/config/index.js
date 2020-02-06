bcef4ef4563b9c7abf911f07508b9c71b11c2226
Daniel Aguilar <dacmail@gmail.com>
2020-02-06 16:30:21 +0100

    topics shortnames in config

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
   SHORTNAME: process.env.SHORTNAME || 'p2030',
-  URL: process.env.URL || 'http://localhost:5000',
+  URL: process.env.URL || 'https://api.dev.parlamento2030.es',
   GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS || 'XXXXXXXXXXXX',
   USE_ALERTS: process.env.USE_ALERTS || false,
   SCANNER_HELPTEXT: process.env.SCANNER_HELPTEXT || "",
@@ -108,70 +108,87 @@ export default {
   STYLES: {
     topics: {
       "ODS 1 - Fin de la Pobreza": {
+        shortname: "ODS 1",
         color: "#eb1c2d",
         image: "S_SDG-goals_icons-individual-rgb-01.png",
       },
       "ODS 2 - Hambre Cero": {
+        shortname: "ODS 2",
         color: "#d3a029",
         image: "S_SDG-goals_icons-individual-rgb-02.png",
       },
       "ODS 3 - Salud y bienestar": {
+        shortname: "ODS 3",
         color: "#4c9f38",
         image: "S_SDG-goals_icons-individual-rgb-03.png",
       },
       "ODS 4 - Educación de calidad": {
+        shortname: "ODS 4",
         color: "#c52333",
         image: "S_SDG-goals_icons-individual-rgb-04.png",
       },
       "ODS 5 - Igualdad de género": {
+        shortname: "ODS 5",
         color: "#ed4135",
         image: "S_SDG-goals_icons-individual-rgb-05.png",
       },
       "ODS 6 - Agua limpia y saneamiento": {
+        shortname: "ODS 6",
         color: "#00aed9",
         image: "S_SDG-goals_icons-individual-rgb-06.png",
       },
       "ODS 7 - Energía asequible y no contaminante": {
+        shortname: "ODS 7",
         color: "#fdb713",
         image: "S_SDG-goals_icons-individual-rgb-07.png",
       },
       "ODS 8 - Trabajo decente y crecimiento económico": {
+        shortname: "ODS 8",
         color: "#8f1838",
         image: "S_SDG-goals_icons-individual-rgb-08.png",
       },
       "ODS 9 - Industria, innovación e infraestructura": {
+        shortname: "ODS 9",
         color: "#f06a38",
         image: "S_SDG-goals_icons-individual-rgb-09.png",
       },
       "ODS 10 - Reducción de las desigualdades": {
+        shortname: "ODS 10",
         color: "#dd1367",
         image: "S_SDG-goals_icons-individual-rgb-10.png",
       },
       "ODS 11 - Ciudades y comunidades sostenibles": {
+        shortname: "ODS 11",
         color: "#f69c39",
         image: "S_SDG-goals_icons-individual-rgb-11.png",
       },
       "ODS 12 - Producción y consumo responsables": {
+        shortname: "ODS 12",
         color: "#cf8d2a",
         image: "S_SDG-goals_icons-individual-rgb-12.png",
       },
       "ODS 13 - Acción por el clima": {
+        shortname: "ODS 13",
         color: "#48773e",
         image: "S_SDG-goals_icons-individual-rgb-13.png",
       },
       "ODS 14 - Vida submarina": {
+        shortname: "ODS 14",
         color: "#007dbc",
         image: "S_SDG-goals_icons-individual-rgb-14.png",
       },
       "ODS 15 - Vida de ecosistemas terrestres": {
+        shortname: "ODS 15",
         color: "#5cb84d",
         image: "S_SDG-goals_icons-individual-rgb-15.png",
       },
       "ODS 16 - Paz, justicia e instituciones sólidas": {
+        shortname: "ODS 16",
         color: "#02558b",
         image: "S_SDG-goals_icons-individual-rgb-16.png",
       },
       "ODS 17 - Alianzas para lograr los objetivos": {
+        shortname: "ODS 17",
         color: "#183668",
         image: "S_SDG-goals_icons-individual-rgb-17.png",
       },
