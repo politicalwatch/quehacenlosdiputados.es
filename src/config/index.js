8009a460a63d3a0279ba579f50d199a951f12cf2
Víctor Ramírez de la Corte <virako.9@gmail.com>
2020-02-17 10:09:28 +0100

    Add log with sentry

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 export default {
-  SHORTNAME: process.env.SHORTNAME || 'p2030',
-  URL: process.env.URL || 'http://localhost:5000',
-  GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS || 'XXXXXXXXXXXX',
-  USE_ALERTS: process.env.USE_ALERTS || false,
-  SCANNER_HELPTEXT: process.env.SCANNER_HELPTEXT || "",
+  SHORTNAME: process.env.VUE_APP_SHORTNAME || 'p2030',
+  URL: process.env.VUE_APP_URL || 'http://localhost:5000',
+  GOOGLE_ANALYTICS: process.env.VUE_APP_GOOGLE_ANALYTICS || 'XXXXXXXXXXXX',
+  USE_ALERTS: process.env.VUE_APP_USE_ALERTS || false,
+  SCANNER_HELPTEXT: process.env.VUE_APP_SCANNER_HELPTEXT || "",
   MENU: [
     {
       route : 'search',
--- src/main.js
+++ src/main.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
+import config from '@/config';
+
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
 
 import './registerServiceWorker'
+
+
+let SENTRY_DSN = process.env.VUE_APP_SENTRY_DSN
+if (SENTRY_DSN) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    integrations: [new Integrations.Vue({Vue, attachProps: true})],
+  })
+}
 
 Vue.config.productionTip = false
 
