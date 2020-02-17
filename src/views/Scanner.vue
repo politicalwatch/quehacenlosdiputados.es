99bb226e15c6ddd1826ada475e00720fc4e6dbcd
Daniel Aguilar <dacmail@gmail.com>
2020-02-17 11:10:58 +0100

    scanner basic structure

--- src/views/Scanner.vue
+++ src/views/Scanner.vue
@@ -1,68 +1,52 @@
 <template>
   <div>
-    <tipi-header title="Scanner"/>
-    <div id="scanner">
-      <div class="container page">
-        <div class="row">
-          <div class="col-sm-7">
-            <textarea placeholder="Inserta aqui el texto que quieres escanear..." v-model="inputText">
-            </textarea>
-            <a id="start" class="btn btn-custom btn-block" @click.prevent="annotate">Iniciar proceso</a>
-            <div class="text-center clean-text">
-              <a id="clean" :class="{ disabled: inProgress }" v-if="inputText!=''" @click="cleanTextAndResult"><i class="fa fa-times"></i> Limpiar texto <span v-if="result">y resultados</span></a>
-            </div>
+    <div id="scanner" class="o-container o-section">
+      <tipi-header title="Scanner"/>
+      <div class="o-grid">
+        <div class="o-grid__col u-12 u-7@sm">
+          <textarea placeholder="Inserta aqui el texto que quieres escanear..." v-model="inputText" />
+          <p>
+            <a id="start" class="c-button c-button--primary" @click.prevent="annotate">Iniciar proceso</a>
+            <a class="c-button" :class="{ disabled: inProgress }" v-if="inputText!=''" @click="cleanTextAndResult">Limpiar texto <span v-if="result">y resultados</span></a>
+          </p>
 
-          </div>
-          <div class="col-sm-5">
-            <p class="helptext" v-html="config.SCANNER_HELPTEXT"></p>
-          </div>
         </div>
+        <div class="o-grid__col u-12 u-5@sm">
+          <p class="helptext" v-html="config.SCANNER_HELPTEXT"></p>
+        </div>
+      </div>
 
-        <div id="result" class="row">
-          <div class="col-sm-12 result" v-if="result">
-            <h2>Resultado del escáner:</h2>
-            <div class="nodata-result" v-if="!result.topics.length">
-              <div class="row">
-                <div class="col-sm-7">
-                  <div class="alert alert-dismissible alert-danger" role="alert">
-                    No hemos encontrado ninguna coincidencia entre tu texto y nuestras etiquetas.
-                  </div>
-                </div>
+      <div id="result" class="o-section o-grid">
+        <div class="o-grid__col u-12 result" v-if="result">
+          <h3>Resultado del escáner:</h3>
+            <tipi-message v-if="!result.topics.length" type="error" icon>No hemos encontrado ninguna coincidencia entre tu texto y nuestras etiquetas.</tipi-message>
+            <div class="o-grid" v-else>
+              <div class="o-grid__col u-12 u-7@sm">
+                <tipi-neuron
+                  v-if="fakeInitiative && allTopics"
+                  :initiative="fakeInitiative"
+                  :topics="allTopics"
+                  :styles="styles"
+                />
               </div>
-            </div>
-            <div class="data-result" v-else>
-              <div class="row">
-                <div class="col-sm-7">
-                  <tipi-neuron
-                    v-if="fakeInitiative && allTopics"
-                    :initiative="fakeInitiative"
-                    :topics="allTopics"
-                    :styles="styles"
-                  />
-                </div>
-                <div class="col-sm-5">
-                  <p class="helptext">Aquí tienes una una relación visual de tu texto, para que de un primer vistazo veas conexiones interesantes.</p>
-                </div>
+              <div class="o-grid__col u-12 u-5@sm">
+                <p>Aquí tienes una una relación visual de tu texto, para que de un primer vistazo veas conexiones interesantes.</p>
               </div>
-              <div class="row">
-                <div class="col-sm-7">
-                  <tipi-topics meta="ODS tratados" :topics="result.topics" :tags="result.tags"/>
-                </div>
-                <div class="col-sm-5">
-                  <p class="helptext">Si haces clic en cualquiera de las etiquetas relacionadas con tu texto podrás conocer además toda la actividad parlamentaria asociada con dicha etiqueta.</p>
-                </div>
+              <div class="o-grid__col u-12 u-7@sm">
+                <tipi-topics meta="ODS tratados" :topics="result.topics" :tags="result.tags"/>
               </div>
-              <div class="row">
-                <div class="col-sm-12 text-center">
-                  <tipi-csv-download
-                    :initiatives="csvItems || []"
-                    :csvItems="csvItems"
-                    :csvFields="csvFields"
-                    :canDownloadCSV="true"
-                    class="btn btn-custom btn-naked"
-                    label="Descárgalo en CSV"
-                  />
-                </div>
+              <div class="o-grid__col u-12 u-5@sm">
+                <p>Si haces clic en cualquiera de las etiquetas relacionadas con tu texto podrás conocer además toda la actividad parlamentaria asociada con dicha etiqueta.</p>
+              </div>
+              <div class="o-grid__col u-12 u-text-center u-padding-top-4">
+                <tipi-csv-download
+                  :initiatives="csvItems || []"
+                  :csvItems="csvItems"
+                  :csvFields="csvFields"
+                  :canDownloadCSV="true"
+                  button-class="c-button--primary"
+                  label="Descárgalo en CSV"
+                />
               </div>
             </div>
           </div>
@@ -73,7 +57,7 @@
 </template>
 
 <script>
-import { TipiHeader, TipiTopics, TipiNeuron, TipiCsvDownload } from 'tipi-uikit'
+import { TipiMessage, TipiHeader, TipiTopics, TipiNeuron, TipiCsvDownload } from 'tipi-uikit'
 import config from '@/config';
 import api from '@/api';
 import { mapState } from 'vuex';
@@ -87,6 +71,7 @@ export default {
     TipiTopics,
     TipiNeuron,
     TipiCsvDownload,
+    TipiMessage,
   },
   data() {
     return {
@@ -145,32 +130,3 @@ export default {
   }
 }
 </script>
-
-<style scoped lang="scss">
-a {
-  cursor: pointer;
-}
-.clean-text {
-  padding-top: 10px;
-}
-.helptext {
-  border-left: 2px solid #3c3c3c;
-  padding-left: 15px;
-  text-align: justify;
-}
-textarea {
-  width: 100%;
-  height: 250px;
-  font-size: 1.1em;
-  border: 0px;
-  padding: 10px;
-}
-#result {
-  min-height: 500px;
-  padding: 20px 0px;
-}
-a.disabled {
-  cursor: not-allowed;
-  opacity: 0.5;
-}
-</style>
