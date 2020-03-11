fb0f7dc49e3595f4d3860451df3be4fc42bee7a9
pr3ssh <x@pr3ssh.net>
2020-03-11 12:49:53 +0100

    Update topic names on config

--- src/config/index.js
+++ src/config/index.js
@@ -107,87 +107,87 @@ export default {
   ],
   STYLES: {
     topics: {
-      "ODS 1 - Fin de la Pobreza": {
+      "ODS 1 Fin de la pobreza": {
         shortname: "ODS 1",
         color: "#eb1c2d",
         image: "ods-1.svg",
       },
-      "ODS 2 - Hambre Cero": {
+      "ODS 2 Hambre cero": {
         shortname: "ODS 2",
         color: "#d3a029",
         image: "ods-2.svg",
       },
-      "ODS 3 - Salud y bienestar": {
+      "ODS 3 Salud y bienestar": {
         shortname: "ODS 3",
         color: "#4c9f38",
         image: "ods-3.svg",
       },
-      "ODS 4 - Educación de calidad": {
+      "ODS 4 Educación de calidad": {
         shortname: "ODS 4",
         color: "#c52333",
         image: "ods-4.svg",
       },
-      "ODS 5 - Igualdad de género": {
+      "ODS 5 Igualdad de género": {
         shortname: "ODS 5",
         color: "#ed4135",
         image: "ods-5.svg",
       },
-      "ODS 6 - Agua limpia y saneamiento": {
+      "ODS 6 Agua limpia y saneamiento": {
         shortname: "ODS 6",
         color: "#00aed9",
         image: "ods-6.svg",
       },
-      "ODS 7 - Energía asequible y no contaminante": {
+      "ODS 7 Energía asequible y no contaminante": {
         shortname: "ODS 7",
         color: "#fdb713",
         image: "ods-7.svg",
       },
-      "ODS 8 - Trabajo decente y crecimiento económico": {
+      "ODS 8 Trabajo decente y crecimiento económico": {
         shortname: "ODS 8",
         color: "#8f1838",
         image: "ods-8.svg",
       },
-      "ODS 9 - Industria, innovación e infraestructura": {
+      "ODS 9 Industria, innovación e infraestructura": {
         shortname: "ODS 9",
         color: "#f06a38",
         image: "ods-9.svg",
       },
-      "ODS 10 - Reducción de las desigualdades": {
+      "ODS 10 Reducción de las desigualdades": {
         shortname: "ODS 10",
         color: "#dd1367",
         image: "ods-10.svg",
       },
-      "ODS 11 - Ciudades y comunidades sostenibles": {
+      "ODS 11 Ciudades y comunidades sostenibles": {
         shortname: "ODS 11",
         color: "#f69c39",
         image: "ods-11.svg",
       },
-      "ODS 12 - Producción y consumo responsables": {
+      "ODS 12 Producción y consumo responsables": {
         shortname: "ODS 12",
         color: "#cf8d2a",
         image: "ods-12.svg",
       },
-      "ODS 13 - Acción por el clima": {
+      "ODS 13 Acción por el clima": {
         shortname: "ODS 13",
         color: "#48773e",
         image: "ods-13.svg",
       },
-      "ODS 14 - Vida submarina": {
+      "ODS 14 Vida submarina": {
         shortname: "ODS 14",
         color: "#007dbc",
         image: "ods-14.svg",
       },
-      "ODS 15 - Vida de ecosistemas terrestres": {
+      "ODS 15 Vida de ecosistemas terrestres": {
         shortname: "ODS 15",
         color: "#5cb84d",
         image: "ods-15.svg",
       },
-      "ODS 16 - Paz, justicia e instituciones sólidas": {
+      "ODS 16 Paz, justicia e instituciones sólidas": {
         shortname: "ODS 16",
         color: "#02558b",
         image: "ods-16.svg",
       },
-      "ODS 17 - Alianzas para lograr los objetivos": {
+      "ODS 17 Alianzas para lograr los objetivos": {
         shortname: "ODS 17",
         color: "#183668",
         image: "ods-17.svg",
