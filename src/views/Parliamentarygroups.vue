0f25aeea1ee8c5f7fe0d906f197def544c902515
pr3ssh <x@pr3ssh.net>
2021-10-14 16:16:34 +0200

    Adds Parliamentary groups skelethon view

---
+++ src/views/Parliamentarygroups.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="parliamentarygroups" class="o-container o-section u-margin-bottom-10">
+    <tipi-header :title="'Grupos parlamentarios'" />
+      <tipi-text
+          :value="allParliamentaryGroups"
+          type="parliamentarygroup"
+          :source="allParliamentaryGroups" />
+  </div>
+</template>
+
+<script>
+import { TipiHeader, TipiText } from 'tipi-uikit'
+import { mapState } from 'vuex';
+
+export default {
+  name: 'parliamentarygroups',
+  components: {
+    TipiHeader,
+    TipiText,
+  },
+  data: function() {
+    return {
+      groups: []
+    }
+  },
+  computed: {
+    ...mapState(['allParliamentaryGroups'])
+  }
+}
+</script>
