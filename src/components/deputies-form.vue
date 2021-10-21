55436e1ec1fa2ce02bfeb550179e3de72ee9eaf1
David Hernandez <davidhernandezruiz@gmail.com>
2021-10-21 16:58:10 +0200

    Adds filtering and ordering options to the deputies view.

---
+++ src/components/deputies-form.vue
@@ -0,0 +1,102 @@
+<template>
+  <form id="deputies-form" class="u-margin-bottom-4 u-border-bottom" role="form" @submit.prevent="preventSubmission($event)">
+    <div class="o-grid">
+      <div class="o-grid__col u-12 u-padding-bottom-4">
+        <div class="c-input-label u-block">
+          <label for="name">Nombre</label>
+          <input v-model="form.name" type="text" id="name" name="name" @input="emitFilters()" placeholder="Nota: Se admiten expresiones regulares">
+        </div>
+      </div>
+    </div>
+    <div class="o-grid">
+      <div class="o-grid__col u-12 u-6@sm u-padding-bottom-4">
+        <div class="c-select-label u-block">
+          <label for="group">Grupo</label>
+          <multiselect
+            selectedLabel="Seleccionado"
+            selectLabel=""
+            deselectLabel="Pulsa para deseleccionar"
+            v-model="form.group"
+            :options="groups"
+            :allow-empty="true"
+            @input="emitFilters()"
+            name="group" id="group" placeholder="Todos">
+          </multiselect>
+        </div>
+      </div>
+      <div class="o-grid__col u-12 u-6@sm u-padding-bottom-4">
+        <div class="c-select-label u-block">
+          <label for="constituency">Circunscripción</label>
+          <multiselect
+            selectedLabel="Seleccionado"
+            selectLabel=""
+            deselectLabel="Pulsa para deseleccionar"
+            v-model="form.constituency"
+            :options="getConstituencies()"
+            :allow-empty="true"
+            @input="emitFilters()"
+            name="constituency" id="constituency" placeholder="Todos">
+          </multiselect>
+        </div>
+      </div>
+    </div>
+    <div class="o-grid">
+      <div class="o-grid__col u-4 u-6@sm u-padding-bottom-4">
+        <button class="c-button u-padding-left-0 u-margin-right-2" @click.prevent="cleanForm">Limpiar búsqueda</button>
+      </div>
+      <div class="o-grid__col u-8 u-6@sm u-padding-bottom-4 u-text-right@sm">
+        <button class="c-button u-padding-right-0" @click.prevent="changeOrder">Ordenar alfabéticamente</button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+import Multiselect from 'vue-multiselect'
+import { watch, ref } from 'vue'
+
+export default {
+  name: 'deputiesForm',
+  components: {
+    Multiselect,
+  },
+  data: function() {
+    return {
+      form: {
+        sortUp: true,
+      },
+    }
+  },
+  props: {
+    deputies: Array,
+    groups: Array,
+  },
+  methods: {
+    cleanForm: function() {
+      this.form.group = null
+      this.form.constituency = null
+      this.form.name = ''
+      this.form.sortUp = true
+
+      this.emitFilters()
+    },
+    emitFilters: function() {
+      this.$emit('setFilters', this.form);
+    },
+    getConstituencies: function() {
+      const constituencies = {}
+      for (const deputy of this.deputies) {
+        constituencies[deputy['constituency']] = deputy['constituency']
+      }
+      return Object.keys(constituencies)
+    },
+    preventSubmission: function(e) {
+      e.preventDefault()
+    },
+    changeOrder: function() {
+      this.form.sortUp = !this.form.sortUp
+      this.emitFilters()
+    },
+  }
+}
+</script>
--- src/views/Deputies.vue
+++ src/views/Deputies.vue
@@ -1,27 +1,88 @@
 <template>
   <div id="deputies" class="o-container o-section u-margin-bottom-10">
     <tipi-header :title="'Lista de diputados'" subtitle="Busca entre todos los diputados para conocer qué hacen en el congreso" />
-    <tipi-text meta="Diputadas/os" :value="allDeputies" type="deputy" :source="allDeputies" />
+    <deputies-form :deputies="deputies" :groups="getGroupsLongNames()" @setFilters="setFilters"></deputies-form>
+    <tipi-text meta="Diputadas/os" :value="getFilteredDeputies()" type="deputy" :source="deputies" />
   </div>
 </template>
 
 <script>
 import { TipiHeader, TipiText } from 'tipi-uikit'
-import { mapState } from 'vuex';
+import api from '@/api'
+import { mapGetters } from 'vuex';
+import DeputiesForm from '@/components/deputies-form';
 
 export default {
   name: 'deputies',
   components: {
+    DeputiesForm,
     TipiHeader,
     TipiText
   },
   data: function() {
     return {
-      deputies: []
+      deputies: [],
+      filters: {},
     }
   },
   computed: {
-    ...mapState(['allDeputies'])
+    ...mapGetters({
+      groups: 'allParliamentaryGroupsWithGoverment',
+    }),
+  },
+  methods: {
+    loadDeputies: function() {
+      api.getDeputies()
+        .then(response => {
+          this.deputies = response
+        })
+        .catch(error => console.log(error))
+    },
+    getGroupsLongNames: function() {
+      return this.groups.map(group => group.name || group)
+    },
+    getGroupShortName: function (longname) {
+      const group = this.groups.filter(group => group['name'] == longname).pop()
+      return group['shortname'].toUpperCase()
+    },
+    setFilters: function(filters) {
+      this.filters = filters
+    },
+    getFilteredDeputies: function() {
+      let filteredDeputies = this.deputies
+
+      if ('constituency' in this.filters && this.filters['constituency'] != null) {
+        filteredDeputies = filteredDeputies.filter(deputy => deputy['constituency'] == this.filters['constituency'])
+      }
+
+      if ('group' in this.filters && this.filters['group'] != null) {
+        filteredDeputies = filteredDeputies.filter(deputy => deputy['parliamentarygroup'].toUpperCase() == this.getGroupShortName(this.filters['group']))
+      }
+
+      if ('name' in this.filters && this.filters['name'] != null) {
+        const regex = new RegExp('.*' + this.filters['name'] + '.*', 'i')
+        filteredDeputies = filteredDeputies.filter(deputy => regex.test(deputy['name']))
+      }
+
+      return [...filteredDeputies].sort((a, b) => {
+        if (!this.filters['sortUp']) {
+          return this.prepareForSorting(a.name) > this.prepareForSorting(b.name)
+        }
+        return this.prepareForSorting(a.name) < this.prepareForSorting(b.name)
+      })
+    },
+    prepareForSorting: function(name) {
+      return name.toUpperCase()
+        .replace('Á', 'A')
+        .replace('É', 'E')
+        .replace('Í', 'I')
+        .replace('Ó', 'O')
+        .replace('Ú', 'U')
+        .replace('Ñ', 'NZ')
+    },
+  },
+  beforeMount() {
+    this.loadDeputies()
   }
 }
 </script>
