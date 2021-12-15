9ea1c46e5fad07c9fccfef609594f42860e177ab
pr3ssh <x@pr3ssh.net>
2021-12-15 15:22:14 +0100

    Only shows active deputies on parliamentarygroup page

--- src/views/Parliamentarygroup.vue
+++ src/views/Parliamentarygroup.vue
@@ -60,7 +60,7 @@ export default {
     ...mapGetters(['getDeputiesByParliamentaryGroup']),
     deputies: function () {
       if (this.parliamentarygroup) {
-        return this.getDeputiesByParliamentaryGroup(this.parliamentarygroup.shortname).map(deputy => deputy);
+          return this.getDeputiesByParliamentaryGroup(this.parliamentarygroup.shortname).filter(deputy => deputy.active).map(deputy => deputy);
       }
       return [];
     },
