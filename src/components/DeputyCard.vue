61a61fa488db5e101088f81ce874e6883306f7ce
pr3ssh <x@pr3ssh.net>
2022-02-04 07:51:54 +0100

    Adds deputy link to component

--- src/components/DeputyCard.vue
+++ src/components/DeputyCard.vue
@@ -2,16 +2,17 @@
   <div :class="'c-deputy-card ' + layout + '-layout'">
     <div class="c-deputy-card__wrapper">
 
-      <!-- <router&#45;link :to="{name: 'deputy', params: {id: deputy.id }}"> -->
+      <router-link :to="{name: 'deputy', params: {id: deputy.id }}">
         <img :src="deputy.image" :title="deputy.name" :class="'c-deputy-card__wrapper__image c-deputy-card__wrapper__image-'+layout" />
+      </router-link>
 
-        <div v-if="layout != 'small'" class="c-deputy-card__wrapper__info">
+      <div v-if="layout != 'small'" class="c-deputy-card__wrapper__info">
+        <router-link :to="{name: 'deputy', params: {id: deputy.id }}">
           <h4>{{deputy.name}}</h4>
-          <p>{{ deputy.parliamentarygroup }}</p>
-          <h5 v-if="layout == 'large'"><icon icon="location"/>{{ deputy.constituency }}</h5>
-        </div>
-
-      <!-- </router&#45;link> -->
+        </router-link>
+        <p>{{ deputy.parliamentarygroup }}</p>
+        <h5 v-if="layout == 'large'"><icon icon="location"/>{{ deputy.constituency }}</h5>
+      </div>
 
     </div>
   </div>
