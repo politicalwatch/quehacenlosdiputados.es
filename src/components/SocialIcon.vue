30c41d1e863310df6353e7b38dfffb8f28e57e47
David Hernández <davidhernandezruiz@gmail.com>
2022-02-08 18:15:10 +0100

    Adds component to render the social media icons.

---
+++ src/components/SocialIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <a class="c-social-icon" :href="link" target="_blank"><img :src="`/img/${icon}.svg`"/></a>
+</template>
+
+<script>
+
+import Icon from '@/components/Icon';
+
+export default {
+  name: 'social-icon',
+  components: {
+    Icon,
+  },
+  props: {
+    link: String,
+    icon: String,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
