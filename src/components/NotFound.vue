04f0768e0cdf03f931eef8afa726f190729f6982
pr3ssh <x@pr3ssh.net>
2022-02-08 11:21:25 +0100

    Implements NotFound component

---
+++ src/components/NotFound.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="c-not-found o-container">
+    <div class="o-grid u-text-center">
+      <div class="o-grid__col u-12@sm">
+        <span v-html="svg"></span>
+      </div>
+      <div class="o-grid__col u-12@sm">
+        <h5>{{ message }}</h5>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+  props: {
+    message: String,
+  },
+  computed: {
+    svg() {
+      let svg = '';
+      try {
+        svg = require(`!html-loader!../assets/svg/not-found.svg`);
+      } catch (error) {
+        svg = '';
+      }
+      return svg;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+h5 {
+  font-size: 16px;
+  margin-top: 32px;
+}
+</style>
