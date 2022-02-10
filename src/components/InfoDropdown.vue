1b90a1986b6484997d9661877bd8b17d92950ccb
David Hernández <davidhernandezruiz@gmail.com>
2022-02-10 12:29:59 +0100

    Adds new component dropdown to display info.

---
+++ src/components/InfoDropdown.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="c-info-dropdown">
+    <div class="c-info-dropdown__title_wrapper" @click="shown = !shown">
+      <h3 class="c-info-dropdown__title">{{ title }}</h3>
+      <div v-show="shown" class="c-info-dropdown__icon">Mostrar menos<icon icon="less"/></div>
+      <div v-show="!shown" class="c-info-dropdown__icon">Mostrar más<icon icon="plus"/></div>
+    </div>
+    <div class="c-into-dropdown__content" v-if="shown">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Icon from '@/components/Icon';
+
+export default {
+  name: 'info-dropdown',
+  components: {
+    Icon,
+  },
+  data: function() {
+    return {
+      shown: false,
+    }
+  },
+  props: {
+    title: String,
+  },
+}
+</script>
+
+<style></style>
