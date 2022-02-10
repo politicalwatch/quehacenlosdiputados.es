4f3a67f6e1916853ee851ea265a7350e430348b1
David Hernández <davidhernandezruiz@gmail.com>
2022-02-10 12:28:11 +0100

    Fixes error on H tags in the parlamentary group card.

--- src/components/ParliamentaryGroupCard.vue
+++ src/components/ParliamentaryGroupCard.vue
@@ -2,24 +2,24 @@
   <div :class="'c-parliamentary_group_card c-parliamentary_group_card__' + layout">
       <div class="c-parliamentary_group_card__overlay" :style="getBackground()"></div>
       <div v-if="layout == 'large'" class="c-parliamentary_group_card__wrapper">
-        <h1>{{getGroupName()}}</h1>
-        <h3><icon icon="person" class="c-icon--secondary c-icon--white" />{{parliamentary_group.composition.deputies}} Diputados/as</h3>
+        <h1 class="u-uppercase">{{getGroupName()}}</h1>
+        <h3 class="u-uppercase"><icon icon="person" class="c-icon--secondary c-icon--white" />{{parliamentary_group.composition.deputies}} Diputados/as</h3>
         <div class="c-parliamentary_group_card__icons">
           <PartyLogoIcon v-for="party in parliamentary_group.parties" :key="party.name" :logo="party.logo" :color="party.color" :name="party.name" />
         </div>
       </div>
       <div v-if="layout == 'medium'" class="c-parliamentary_group_card__wrapper">
         <router-link :to="{name: 'parliamentarygroup', params: {id: parliamentary_group.id }}">
-          <h3>{{getGroupName()}}</h3>
-          <h4><icon icon="person" class="c-icon--secondary c-icon--white" />{{parliamentary_group.composition.deputies}} Diputados/as</h4>
+          <h3 class="u-uppercase">{{getGroupName()}}</h3>
+          <h4 class="u-uppercase"><icon icon="person" class="c-icon--secondary c-icon--white" />{{parliamentary_group.composition.deputies}} Diputados/as</h4>
           <div class="c-parliamentary_group_card__icons">
-            <PartyLogoIcon v-for="party in parliamentary_group.parties" :key="party.name" :logo="party.logo" :color="party.color" :name="party.name" />
+            <PartyLogoIcon v-for="party in parliamentary_group.parties" :key="party.name" :party="party.name" />
           </div>
         </router-link>
       </div>
       <div v-if="layout == 'small'" class="c-parliamentary_group_card__wrapper">
         <router-link :to="{name: 'parliamentarygroup', params: {id: parliamentary_group.id }}">
-          <h4>{{getGroupName()}}</h4>
+          <h4 class="u-uppercase">{{getGroupName()}}</h4>
         </router-link>
       </div>
   </div>
