1f4372109a86a933a88c068b826bff9029fdacae
pr3ssh <x@pr3ssh.net>
2022-03-07 20:34:01 +0100

    Adds groups radiography

---
+++ src/components/Gender.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="c-gender">
+    <div class="c-gender__icon">
+      <icon :icon="gender"></icon>
+    </div>
+    <div class="c-gender__info">
+      <div class="c-gender__info__percentage">
+        {{ percentage }}%
+      </div>
+      <div class="c-gender__info__name">
+        {{ getNameFromType(gender) }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from './Icon';
+
+export default {
+  name: "gender",
+  props: {
+    gender: {
+      type: String, // female, male
+      required: true,
+    },
+    percentage: {
+      type: Number,
+      required: true
+    },
+  },
+  components: {
+    Icon
+  },
+  methods: {
+    getNameFromType: function(type) {
+      if (type == 'female') return 'Mujeres'
+      if (type == 'male') return 'Hombres'
+      return ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/views/Parliamentarygroup.vue
+++ src/views/Parliamentarygroup.vue
@@ -4,7 +4,24 @@
     <div v-if="parliamentarygroup" id="group" class="o-container o-section u-margin-bottom-10">
       <ParliamentaryGroupCard :parliamentary_group="parliamentarygroup" layout="large"/>
 
-      <h2 class="u-uppercase u-margin-bottom-4" v-if="latestInitiatives && latestInitiatives.length">Últimas iniciativas</h2>
+      <div class="o-container" v-if="parliamentarygroup">
+        <h2 class="u-uppercase u-margin-bottom-4">Radiografía</h2>
+        <div class="o-grid">
+          <div class="o-grid__col u-12 u-4@md">
+            <gender gender="female" :percentage="calculatePercentage(parliamentarygroup.composition.gender.female)" />
+            <gender gender="male" :percentage="calculatePercentage(parliamentarygroup.composition.gender.male)" />
+          </div>
+          <div class="o-grid__col u-12 u-8@md">
+            <h3>Distribución por edades</h3>
+            <div><span class="u-text-th3">{{ calculatePercentage(parliamentarygroup.composition.ages.under35) }}%</span> con menos de 35 años</div>
+            <div><span class="u-text-th3">{{ calculatePercentage(parliamentarygroup.composition.ages.between35and49) }}%</span> entre 35 y 49 años</div>
+            <div><span class="u-text-th3">{{ calculatePercentage(parliamentarygroup.composition.ages.between50and65) }}%</span> entre 50 y 65 años</div>
+            <div><span class="u-text-th3">{{ calculatePercentage(parliamentarygroup.composition.ages.over65) }}%</span> con más de 65 años</div>
+          </div>
+        </div>
+      </div>
+
+      <h2 class="u-uppercase u-margin-top-8 u-margin-bottom-4" v-if="latestInitiatives && latestInitiatives.length">Últimas iniciativas</h2>
       <results layout="tiny" :initiatives="latestInitiatives" class="u-margin-bottom-4" :topicsStyles="topicsStyles"/>
 
       <save-alert :searchparams="{author: parliamentarygroup.name}" :text="parliamentarygroup.name"/>
@@ -26,6 +43,7 @@ import CardGrid from '@/components/CardGrid';
 import ParliamentaryGroupCard from '@/components/ParliamentaryGroupCard';
 import Results from '@/components/Results'
 import CustomText from '@/components/CustomText';
+import Gender from '@/components/Gender';
 import Loader from '@/components/Loader';
 import SaveAlert from '@/components/SaveAlert';
 import api from '@/api';
@@ -38,6 +56,7 @@ export default {
     CardGrid,
     Results,
     CustomText,
+    Gender,
     Loader,
     SaveAlert,
     ParliamentaryGroupCard,
@@ -88,6 +107,9 @@ export default {
            if (response.initiatives) this.latestInitiatives = response.initiatives;
           })
          .catch(error => this.errors = error);
+    },
+    calculatePercentage: function(value) {
+      return Math.round((value / this.parliamentarygroup.composition.deputies) * 100)
     },
   },
   created: function() {
