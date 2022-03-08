d88dae96fd14c6c78f8d4ce275fd9620dd1b7a4f
pr3ssh <x@pr3ssh.net>
2022-03-08 15:48:49 +0100

    Implements empty searches with alerts CTA

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -6,28 +6,35 @@
 
       <initiatives-form :formData="this.data" @getResults="getResults" @clearInitiatives="clearInitiatives" />
 
-        <div class="o-grid o-grid--align-center u-margin-bottom-4" id="results" v-if="initiatives.length > 0">
-        <div class="o-grid__col o-grid__col--fill">
-          <h2 class="u-uppercase" v-if="this.query_meta.page">{{ message.message }}</h2>
-          <csv-download
-            :initiatives="initiatives || []"
-            :csvItems="csvItems"
-            :canDownloadCSV="canDownloadCSV"
-            @loadCSVItems="loadCSVItems"
-          />
+      <div v-if="initiatives.length > 0">
+        <div class="o-grid o-grid--align-center u-margin-bottom-4" id="results">
+          <div class="o-grid__col o-grid__col--fill">
+            <h2 class="u-uppercase" v-if="this.query_meta.page">{{ message.message }}</h2>
+            <csv-download
+              :initiatives="initiatives || []"
+              :csvItems="csvItems"
+              :canDownloadCSV="canDownloadCSV"
+              @loadCSVItems="loadCSVItems"
+            />
+          </div>
+          <div class="o-grid__col o-grid__col--right">
+            <AlertButton :searchparams="data" v-show="use_alerts && this.query_meta.page" />
+          </div>
         </div>
-        <div class="o-grid__col o-grid__col--right">
-          <AlertButton :searchparams="data" v-show="use_alerts && this.query_meta.page" />
-        </div>
+        <results
+          :loadingResults="loadingResults"
+          :initiatives="initiatives || []"
+          :topicsStyles="topicsStyles"
+          :queryMeta="query_meta"
+          @loadMore="loadMore"
+        />
       </div>
-      <results
-        :loadingResults="loadingResults"
-        :initiatives="initiatives || []"
-        :topicsStyles="topicsStyles"
-        :queryMeta="query_meta"
-        @loadMore="loadMore"
-        v-if="initiatives.length > 0"
-      />
+
+      <div v-else id="results" class="u-padding-top-6 u-text-center">
+        <not-found message="No se han encontrado resultados. Crea una alerta y te avisaremos."/>
+        <AlertButton :searchparams="data" v-show="use_alerts && this.query_meta.page" />
+      </div>
+
     </div>
   </div>
 </template>
@@ -41,6 +48,7 @@ import PageHeader from '@/components/PageHeader';
 import CsvDownload from '@/components/CsvDownload';
 import Message from '@/components/Message';
 import Results from '@/components/Results';
+import NotFound from '@/components/NotFound';
 import { mapGetters } from 'vuex';
 import Vue from 'vue'
 
@@ -54,6 +62,7 @@ export default {
     InitiativesForm,
     PageHeader,
     Results,
+    NotFound,
     Message,
     CsvDownload,
   },
@@ -104,43 +113,43 @@ export default {
       const isNewSearch = event && event.type === 'submit';
       const params = this.$route.params.data && !isNewSearch ?
         qs.parse(this.$route.params.data)
-        : formData;
-      this.data = Object.assign(this.data, params);
-      const urlParams = Object.assign({}, this.data);
+          : formData;
+        this.data = Object.assign(this.data, params);
+        const urlParams = Object.assign({}, this.data);
 
-      if (isNewSearch) {
-        this.scrollToID = '#results';
-        event.preventDefault();
-      }
-
-      Object.keys(urlParams).forEach(
-        key => (!urlParams[key] || key === "page") && delete urlParams[key])
-
-      this.$router.push({
-        name: 'results',
-        params: {
-          data: qs.stringify(urlParams, { arrayFormat: 'repeat' })
+        if (isNewSearch) {
+          this.scrollToID = '#results';
+          event.preventDefault();
         }
-      }).catch(e => e);
 
-      api.getInitiatives(this.data)
-         .then(response => {
-            if (!isNewSearch) {
-              this.initiatives.push.apply(this.initiatives, response.initiatives);
-            } else {
-              this.initiatives = response.initiatives;
+        Object.keys(urlParams).forEach(
+          key => (!urlParams[key] || key === "page") && delete urlParams[key])
+
+          this.$router.push({
+            name: 'results',
+            params: {
+              data: qs.stringify(urlParams, { arrayFormat: 'repeat' })
             }
-            this.query_meta = response.query_meta;
-            this.loadingResults = false;
-            Vue.nextTick()
-              .then(() => {
-                VueScrollTo.scrollTo(this.scrollToID, 1500)
-              })
-          })
-         .catch(error => this.errors = error);
+          }).catch(e => e);
+
+          api.getInitiatives(this.data)
+            .then(response => {
+              if (!isNewSearch) {
+                this.initiatives.push.apply(this.initiatives, response.initiatives);
+              } else {
+                this.initiatives = response.initiatives;
+              }
+              this.query_meta = response.query_meta;
+              this.loadingResults = false;
+              Vue.nextTick()
+                .then(() => {
+                  VueScrollTo.scrollTo(this.scrollToID, 1500)
+                })
+            })
+            .catch(error => this.errors = error);
     },
     clearInitiatives: function(event) {
-        this.initiatives = [];
+      this.initiatives = [];
     },
     loadMore: function() {
       let node = document.querySelectorAll('.c-initiative-card');
@@ -154,11 +163,11 @@ export default {
       event.target.innerText = "Procesando descarga...";
       let params =  Object.assign({ per_page: this.LIMITCSV }, this.data);
       api.getInitiatives(params)
-         .then(response => {
-           this.csvItems = response.initiatives;
-           event.target.innerText = "Descargar resultados";
-          })
-         .catch(error => this.errors = error);
+        .then(response => {
+          this.csvItems = response.initiatives;
+          event.target.innerText = "Descargar resultados";
+        })
+        .catch(error => this.errors = error);
     },
   },
   created: function() {
