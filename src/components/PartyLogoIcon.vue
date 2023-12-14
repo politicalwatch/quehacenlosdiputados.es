4a4574e20a83df924f83bf8a312851eacf393f86
Alex Ahumada <alex.ahumada@politicalwatch.es>
2023-12-14 12:54:41 +0100

    Adds vue-inline-svg to PartyLogoIcon

--- src/components/PartyLogoIcon.vue
+++ src/components/PartyLogoIcon.vue
@@ -5,18 +5,21 @@
     :style="getBackground"
   >
     <figure class="c-party_logo_icon__image" :alt="'Logo de ' + getName">
-      <component :is="svg"></component>
+      <inline-svg :src="svg" />
     </figure>
   </div>
 </template>
 
 <script>
-import { defineAsyncComponent } from "vue";
+import InlineSvg from "vue-inline-svg";
 
 export default {
   name: "PartyLogoIcon",
   props: {
     party: String,
+  },
+  components: {
+    InlineSvg,
   },
   data: function () {
     return {
@@ -27,6 +30,11 @@ export default {
           color: "#ff4f00",
         },
         VOX: {
+          name: "VOX",
+          logo: "vox",
+          color: "#5ac035",
+        },
+        Vox: {
           name: "VOX",
           logo: "vox",
           color: "#5ac035",
@@ -189,12 +197,13 @@ export default {
     svg() {
       let svg = "";
       try {
-        svg = defineAsyncComponent(
-          () =>
-            import(
-              `@/assets/party_logos/icon/${this.parties[this.party].logo}.svg`
-            )
-        );
+        if (this.party in this.parties) {
+          svg = `../assets/party_logos/icon/${
+            this.parties[this.party].logo
+          }.svg`;
+        } else {
+          svg = "../assets/svg/icon-error.svg";
+        }
       } catch (error) {
         svg = this.icon;
       }
