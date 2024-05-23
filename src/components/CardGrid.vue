fa2eff528e1f5cfb0c3e763a45637667d78f827c
Alex Ahumada <alex.ahumada@politicalwatch.es>
2024-05-23 14:41:29 +0200

    Migrates some components to Composition API

--- src/components/CardGrid.vue
+++ src/components/CardGrid.vue
@@ -1,48 +1,50 @@
 <template>
   <div class="o-grid c-card_grid">
-    <div :class="classes" v-for="item in this.items" v-bind:key="item.id">
-      <ParliamentaryGroupCard :parliamentary_group="item" v-if="isParliamentaryGroup()" :layout="layout"/>
-      <deputy-card v-if="isDeputy()" :deputy="item" :layout="layout" :footprint="getDeputyExtra()"/>
+    <div :class="classes" v-for="item in items" v-bind:key="item.id">
+      <ParliamentaryGroupCard
+        :parliamentary_group="item"
+        v-if="isParliamentaryGroup()"
+        :layout="layout"
+      />
+      <deputy-card
+        v-if="isDeputy()"
+        :deputy="item"
+        :layout="layout"
+        :footprint="getDeputyExtra()"
+      />
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
+import { computed } from "vue";
 
-import ParliamentaryGroupCard from '@/components/ParliamentaryGroupCard.vue';
-import DeputyCard from '@/components/DeputyCard.vue';
+import ParliamentaryGroupCard from "@/components/ParliamentaryGroupCard.vue";
+import DeputyCard from "@/components/DeputyCard.vue";
 
-export default {
-  name: 'CardGrid',
-  components: {
-    ParliamentaryGroupCard,
-    DeputyCard,
-  },
-  props: {
-    items: Array,
-    type: String,
-    layout: String,
-    extra: Object,
-  }, 
-  methods: {
-    isDeputy: function() {
-      return this.type == 'deputy'
-    },
-    isParliamentaryGroup: function() {
-      return this.type == 'parliamentarygroup'
-    },
-    getDeputyExtra: function() {
-      if (this.extra === undefined) return 'General'
-      if ('footprint' in this.extra) {
-        return this.extra['footprint']
-      }
-      return 'General'
-    }
-  },
-  computed: {
-    classes: function() {
-      return "o-grid__col u-12 u-4@sm c-card_grid__item c-card_grid__item__" + this.type + " c-card_grid__" + this.layout
-    }
+const { items, type, layout, extra } = defineProps({
+  items: { type: Array },
+  type: { type: String },
+  layout: { type: String },
+  extra: { type: Object },
+});
+
+const isDeputy = () => type == "deputy";
+const isParliamentaryGroup = () => type == "parliamentarygroup";
+const getDeputyExtra = () => {
+  if (extra === undefined) return "General";
+  if ("footprint" in extra) {
+    return extra["footprint"];
   }
-}
+  return "General";
+};
+
+const classes = computed(() => {
+  return (
+    "o-grid__col u-12 u-4@sm c-card_grid__item c-card_grid__item__" +
+    type +
+    " c-card_grid__" +
+    layout
+  );
+});
 </script>
--- src/components/CongressLink.vue
+++ src/components/CongressLink.vue
@@ -1,17 +1,17 @@
 <template>
-  <a :href="url" class="c-congress-link u-uppercase u-border-link" target="_blank" title="Ver en el Congreso.es">Ver en el Congreso.es<icon icon="open-blank"></icon></a>
+  <a
+    :href="url"
+    class="c-congress-link u-uppercase u-border-link"
+    target="_blank"
+    title="Ver en el Congreso.es"
+    >Ver en el Congreso.es<icon icon="open-blank"></icon
+  ></a>
 </template>
 
-<script>
-import Icon from '@/components/Icon.vue';
+<script setup>
+import Icon from "@/components/Icon.vue";
 
-export default {
-  name: 'CongressLink',
-  components: {
-    Icon,
-  },
-  props: {
-    url: String,
-  }
-}
+const { url } = defineProps({
+  url: { type: String },
+});
 </script>
