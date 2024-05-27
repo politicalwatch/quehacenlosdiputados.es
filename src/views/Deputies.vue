199146082cebdac49036f2e1c20ed2210bf3e720
Alex Ahumada <alex.ahumada@politicalwatch.es>
2024-05-27 17:11:45 +0200

    Remove deputies duplicated query

--- src/views/Deputies.vue
+++ src/views/Deputies.vue
@@ -2,173 +2,141 @@
   <div id="deputies" class="o-container o-section u-margin-bottom-10">
     <page-header title="Listado de diputados" />
     <deputies-form
-      :deputies="deputies"
-      :groups="getGroupsLongNames()"
+      :deputies="store.allDeputies"
+      :groups="getGroupsLongNames"
       @setFilters="setFilters"
-      :ranking="getRanking()"
+      :ranking="getRanking"
     ></deputies-form>
     <loader
-      v-if="!this.hasLoadedDeputies"
+      v-if="store.allDeputies.length == 0"
       title="Cargando diputados"
       subtitle="Puede llevar algun tiempo"
     />
     <CardGrid
-      v-bind:key="this.filters['footprint']"
-      :items="getFilteredDeputies()"
+      v-bind:key="filters['footprint']"
+      :items="getFilteredDeputies"
       type="deputy"
       layout="large"
-      :extra="{ footprint: getSelectedFootprint() }"
+      :extra="{ footprint: getSelectedFootprint }"
     />
     <not-found
-      v-if="this.hasLoadedDeputies && getFilteredDeputies().length == 0"
+      v-if="hasLoadedDeputies && getFilteredDeputies.length == 0"
       message="No se han encontrado diputados."
     />
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref, computed } from "vue";
+
 import DeputiesForm from "@/components/DeputiesForm.vue";
 import PageHeader from "@/components/PageHeader.vue";
-import DeputyCard from "@/components/DeputyCard.vue";
 import Loader from "@/components/Loader.vue";
 import CardGrid from "@/components/CardGrid.vue";
 import NotFound from "@/components/NotFound.vue";
-import api from "@/api";
 import { useParliamentStore } from "@/stores/parliament";
 
-export default {
-  name: "deputies",
-  components: {
-    DeputiesForm,
-    PageHeader,
-    DeputyCard,
-    CardGrid,
-    NotFound,
-    Loader,
-  },
-  setup() {
-    const store = useParliamentStore();
-    return { store };
-  },
-  data: function () {
-    return {
-      deputies: [],
-      filters: {},
-      hasLoadedDeputies: false,
-    };
-  },
-  methods: {
-    loadDeputies: function () {
-      api
-        .getDeputies()
-        .then((response) => {
-          this.deputies = response.filter((d) => d.active);
-          this.hasLoadedDeputies = true;
-        })
-        .catch((error) => console.log(error));
-    },
-    getGroupsLongNames: function () {
-      return this.store.allParliamentaryGroups.map(
-        (group) => group.name || group
+const store = useParliamentStore();
+
+const filters = ref({});
+
+const getGroupsLongNames = computed(() => {
+  return store.allParliamentaryGroups.map((group) => group.name || group);
+});
+
+const getGroupShortName = (longname) => {
+  const group = store.allParliamentaryGroups
+    .filter((group) => group["name"] == longname)
+    .pop();
+  return group["shortname"].toUpperCase();
+};
+
+const setFilters = (updatedFilters) => {
+  filters.value = updatedFilters;
+};
+
+const getFilteredDeputies = computed(() => {
+  let filteredDeputies = store.allDeputies;
+
+  if (
+    "constituency" in filters.value &&
+    filters.value["constituency"] != null
+  ) {
+    filteredDeputies = filteredDeputies.filter(
+      (deputy) => deputy["constituency"] == filters.value["constituency"]
+    );
+  }
+
+  if ("group" in filters.value && filters.value["group"] != null) {
+    filteredDeputies = filteredDeputies.filter(
+      (deputy) =>
+        deputy["parliamentarygroup"].toUpperCase() ==
+        getGroupShortName(filters.value["group"])
+    );
+  }
+
+  if ("name" in filters.value && filters.value["name"] != null) {
+    const regex = new RegExp(".*" + filters.value["name"] + ".*", "i");
+    filteredDeputies = filteredDeputies.filter((deputy) =>
+      regex.test(deputy["name"])
+    );
+  }
+
+  if ("footprint" in filters.value && filters.value["footprint"] != undefined) {
+    const footprint_sort = filters.value["footprint"];
+
+    const updated_list = [...filteredDeputies].sort((a, b) => {
+      const a_footprint_filter = a.footprint_by_topics.filter(
+        (item) => item.name == footprint_sort
       );
-    },
-    getGroupShortName: function (longname) {
-      const group = this.store.allParliamentaryGroups
-        .filter((group) => group["name"] == longname)
-        .pop();
-      return group["shortname"].toUpperCase();
-    },
-    setFilters: function (filters) {
-      console.log(filters);
-      this.filters = filters;
-    },
-    getFilteredDeputies: function () {
-      let filteredDeputies = this.deputies;
+      const a_footprint =
+        a_footprint_filter.length > 0 ? a_footprint_filter[0].score : 0;
+      const b_footprint_filter = b.footprint_by_topics.filter(
+        (item) => item.name == footprint_sort
+      );
+      const b_footprint =
+        b_footprint_filter.length > 0 ? b_footprint_filter[0].score : 0;
 
-      if (
-        "constituency" in this.filters &&
-        this.filters["constituency"] != null
-      ) {
-        filteredDeputies = filteredDeputies.filter(
-          (deputy) => deputy["constituency"] == this.filters["constituency"]
-        );
-      }
+      return b_footprint - a_footprint;
+    });
 
-      if ("group" in this.filters && this.filters["group"] != null) {
-        filteredDeputies = filteredDeputies.filter(
-          (deputy) =>
-            deputy["parliamentarygroup"].toUpperCase() ==
-            this.getGroupShortName(this.filters["group"])
-        );
-      }
+    return updated_list;
+  }
+  return [...filteredDeputies].sort((a, b) => {
+    const a_name = prepareForSorting(a.name);
+    const b_name = prepareForSorting(b.name);
 
-      if ("name" in this.filters && this.filters["name"] != null) {
-        const regex = new RegExp(".*" + this.filters["name"] + ".*", "i");
-        filteredDeputies = filteredDeputies.filter((deputy) =>
-          regex.test(deputy["name"])
-        );
-      }
+    return a_name.localeCompare(b_name);
+  });
+});
 
-      if (
-        "footprint" in this.filters &&
-        this.filters["footprint"] != undefined
-      ) {
-        const footprint_sort = this.getSelectedFootprint();
+const getRanking = computed(() => {
+  const ranking = [];
+  for (const item of store.allTopics) {
+    ranking.push(item.name);
+  }
 
-        const updated_list = [...filteredDeputies].sort((a, b) => {
-          const a_footprint_filter = a.footprint_by_topics.filter(
-            (item) => item.name == footprint_sort
-          );
-          const a_footprint =
-            a_footprint_filter.length > 0 ? a_footprint_filter[0].score : 0;
-          const b_footprint_filter = b.footprint_by_topics.filter(
-            (item) => item.name == footprint_sort
-          );
-          const b_footprint =
-            b_footprint_filter.length > 0 ? b_footprint_filter[0].score : 0;
+  return ranking;
+});
 
-          return b_footprint - a_footprint;
-        });
+const getSelectedFootprint = computed(() => {
+  const footprint_sort = filters.value["footprint"];
+  if (footprint_sort == undefined) {
+    return "General";
+  }
+  return footprint_sort;
+});
 
-        return updated_list;
-      }
-      return [...filteredDeputies].sort((a, b) => {
-        const a_name = this.prepareForSorting(a.name);
-        const b_name = this.prepareForSorting(b.name);
+const prepareForSorting = (name) => {
+  const clean_name = name
+    .toUpperCase()
+    .replace("Á", "A")
+    .replace("É", "E")
+    .replace("Í", "I")
+    .replace("Ó", "O")
+    .replace("Ú", "U")
+    .replace("Ñ", "NZ");
 
-        return a_name.localeCompare(b_name);
-      });
-    },
-    getRanking: function () {
-      const ranking = [];
-      for (const item of this.store.allTopics) {
-        ranking.push(item.name);
-      }
-
-      return ranking;
-    },
-    getSelectedFootprint: function () {
-      const footprint_sort = this.filters["footprint"];
-      if (footprint_sort == undefined) {
-        return "General";
-      }
-      return footprint_sort;
-    },
-    prepareForSorting: function (name) {
-      const clean_name = name
-        .toUpperCase()
-        .replace("Á", "A")
-        .replace("É", "E")
-        .replace("Í", "I")
-        .replace("Ó", "O")
-        .replace("Ú", "U")
-        .replace("Ñ", "NZ");
-
-      return clean_name;
-    },
-  },
-  beforeMount() {
-    this.loadDeputies();
-  },
+  return clean_name;
 };
 </script>
