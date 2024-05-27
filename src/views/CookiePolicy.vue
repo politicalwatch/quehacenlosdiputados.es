f14820028ba029d79b96a7f7dfa07a17e00a24f4
Alex Ahumada <alex.ahumada@politicalwatch.es>
2024-05-27 17:12:56 +0200

    Migrates missing views to Composition API

--- src/views/CookiePolicy.vue
+++ src/views/CookiePolicy.vue
@@ -3,23 +3,68 @@
     <div id="legal-notice" class="o-container o-section u-margin-bottom-10">
       <div class="o-grid">
         <div class="o-grid__col u-12 u-4@sm">
-          <page-header title="Política de cookies"/>
+          <page-header title="Política de cookies" />
         </div>
         <div class="o-grid__col u-12 u-8@sm">
-          <p>En cumplimiento con lo establecido en el artículo 22.2 de la Ley de Servicios de la Sociedad de la Información y del Comercio Electrónico (LSSI) y en adecuación con la Directiva Europea 2009/136/CE, Political Watch le informa de que este sitio web hace uso de cookies.</p>
-          <p>Para hacer un uso completo del contenido y las funciones personalizadas de este sitio web, el navegador de su ordenador o dispositivo móvil debe aceptar las cookies. Es común que su navegador lo haga de forma predeterminada.</p>
-          <p>Las cookies de este sitio web no almacenan información confdencial como su nombre, dirección, etc. No obstante, si desea restringir, bloquear o eliminar las cookies de este sitio web o de cualquier otro, puede configurar su navegador para rechazar o aceptar las mismas, o para que estas éstas sean eliminadas de forma automática una vez cierre el navegador o apague el ordenador o dispositivo móvil.</p>
-          <p>La configuración de las cookies en los navegadores se realiza de la siguiente forma:</p>
+          <p>
+            En cumplimiento con lo establecido en el artículo 22.2 de la Ley de
+            Servicios de la Sociedad de la Información y del Comercio
+            Electrónico (LSSI) y en adecuación con la Directiva Europea
+            2009/136/CE, Political Watch le informa de que este sitio web hace
+            uso de cookies.
+          </p>
+          <p>
+            Para hacer un uso completo del contenido y las funciones
+            personalizadas de este sitio web, el navegador de su ordenador o
+            dispositivo móvil debe aceptar las cookies. Es común que su
+            navegador lo haga de forma predeterminada.
+          </p>
+          <p>
+            Las cookies de este sitio web no almacenan información confdencial
+            como su nombre, dirección, etc. No obstante, si desea restringir,
+            bloquear o eliminar las cookies de este sitio web o de cualquier
+            otro, puede configurar su navegador para rechazar o aceptar las
+            mismas, o para que estas éstas sean eliminadas de forma automática
+            una vez cierre el navegador o apague el ordenador o dispositivo
+            móvil.
+          </p>
+          <p>
+            La configuración de las cookies en los navegadores se realiza de la
+            siguiente forma:
+          </p>
           <ul>
             <li>Firefox: Preferencias &gt; Privacidad.</li>
-            <li>Google Chrome: Configuración &gt; Privacidad &gt; Configuración de contenido &gt; Cookies.</li>
+            <li>
+              Google Chrome: Configuración &gt; Privacidad &gt; Configuración de
+              contenido &gt; Cookies.
+            </li>
             <li>Safari: Preferencias &gt; Privacidad</li>
-            <li>Internet Explorer: Herramientas &gt; Opciones de Internet &gt; Privacidad &gt; Avanzada.</li>
+            <li>
+              Internet Explorer: Herramientas &gt; Opciones de Internet &gt;
+              Privacidad &gt; Avanzada.
+            </li>
           </ul>
-          <p>Si comparte contenido de este sitio web mediante redes sociales como Facebook y Twitter, es posible que reciba cookies de dichos sitios web. Los ajustes de las cookies de terceros no están bajo el control de Political Watch, por lo que le sugerimos que compruebe los sitios web de terceros para obtener más información sobre sus cookies y cómo gestionarlas.</p>
-          <p>En <b>Qué hacen los diputados</b> utilizamos las siguientes cookies:</p>
+          <p>
+            Si comparte contenido de este sitio web mediante redes sociales como
+            Facebook y Twitter, es posible que reciba cookies de dichos sitios
+            web. Los ajustes de las cookies de terceros no están bajo el control
+            de Political Watch, por lo que le sugerimos que compruebe los sitios
+            web de terceros para obtener más información sobre sus cookies y
+            cómo gestionarlas.
+          </p>
+          <p>
+            En <b>Qué hacen los diputados</b> utilizamos las siguientes cookies:
+          </p>
           <ul>
-            <li><em>_ga</em>: Esta cookie de Google es utilizada para su servicio de Google Analytics. Estas cookies de Google Analytics generan un ID de usuario anónimo, que es el que se utiliza para hacer recuento de cuantas veces visita el sitio un usuario. También registra cuando fue la primera y la última vez que visitó la web. Asimismo, calcula cuando se ha terminado una sesión, origen del usuario, y keywords.</li>
+            <li>
+              <em>_ga</em>: Esta cookie de Google es utilizada para su servicio
+              de Google Analytics. Estas cookies de Google Analytics generan un
+              ID de usuario anónimo, que es el que se utiliza para hacer
+              recuento de cuantas veces visita el sitio un usuario. También
+              registra cuando fue la primera y la última vez que visitó la web.
+              Asimismo, calcula cuando se ha terminado una sesión, origen del
+              usuario, y keywords.
+            </li>
           </ul>
         </div>
       </div>
@@ -27,18 +72,8 @@
   </div>
 </template>
 
-<script>
-
-  import PageHeader from '@/components/PageHeader.vue';
-
-  export default {
-      name: 'cookie-policy',
-      components: {
-          PageHeader,
-        },
-    }
+<script setup>
+import PageHeader from "@/components/PageHeader.vue";
 </script>
 
-<style scoped lang="scss">
-</style>
-
+<style scoped lang="scss"></style>
--- src/views/LegalNotice.vue
+++ src/views/LegalNotice.vue
@@ -3,13 +3,70 @@
     <div id="legal-notice" class="o-container o-section u-margin-bottom-10">
       <div class="o-grid">
         <div class="o-grid__col u-12 u-4@sm">
-          <page-header title="Aviso legal"/>
+          <page-header title="Aviso legal" />
         </div>
         <div class="o-grid__col u-12 u-8@sm">
-          <table><thead><tr><th>TITULAR DEL SITIO WEB</th></tr></thead><tbody><tr><td><strong>DENOMINACIÓN SOCIAL:</strong> FUNDACIÓN SALVADOR SOLER MUNDO JUSTO</td></tr><tr><td><strong>CIF:</strong> G85959666</td></tr><tr><td><strong>DOMICILIO SOCIAL:</strong> CARDENAL HERRERA ORIA 63, 3 IZDA, 28047, MADRID</td></tr><tr><td><strong>CONTACTO:</strong> <br>Teléfono: 917314873 <br> Email: <a href="mailto:coordinacion@unmundosalvadorsoler.org">coordinacion@unmundosalvadorsoler.org</a></td></tr><tr><td><strong>DATOS DE INSCRIPCIÓN EN EL REGISTRO DE FUNDACIONES:</strong> <br>Inscrita el catorce de diciembre de dos mil diez en el Registro de Fundaciones del Ministerio de Cultura , con el número cronológico 962</td></tr></tbody></table>
+          <table>
+            <thead>
+              <tr>
+                <th>TITULAR DEL SITIO WEB</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <strong>DENOMINACIÓN SOCIAL:</strong> FUNDACIÓN SALVADOR SOLER
+                  MUNDO JUSTO
+                </td>
+              </tr>
+              <tr>
+                <td><strong>CIF:</strong> G85959666</td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>DOMICILIO SOCIAL:</strong> CARDENAL HERRERA ORIA 63, 3
+                  IZDA, 28047, MADRID
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>CONTACTO:</strong> <br />Teléfono: 917314873 <br />
+                  Email:
+                  <a href="mailto:coordinacion@unmundosalvadorsoler.org"
+                    >coordinacion@unmundosalvadorsoler.org</a
+                  >
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong
+                    >DATOS DE INSCRIPCIÓN EN EL REGISTRO DE FUNDACIONES:</strong
+                  >
+                  <br />Inscrita el catorce de diciembre de dos mil diez en el
+                  Registro de Fundaciones del Ministerio de Cultura , con el
+                  número cronológico 962
+                </td>
+              </tr>
+            </tbody>
+          </table>
 
-          <p><b>Qué hacen los diputados</b> es un proyecto de Political Watch que a, su vez, forma parte de la Fundación Salvador Soler (en adelante FSS).</p>
-          <p>FSS cumple con los requisitos Qué hacen los diputados la Ley 34/2002, de 11 de Julio, de Servicios de la Sociedad de la Información y Comercio Electrónico, en la Ley 56/2007, de 28 de diciembre de Medidas de Impulso de la Sociedad de la Información, así como en la Ley Orgánica 15/1999, de 13 de diciembre, sobre Protección de Datos de Carácter Personal y su normativa de desarrollo (RD 1720/2007, de 21 de diciembre, por el que se aprueba el Reglamento de Desarrollo de la Ley Orgánica 15/1999 de Protección de Datos de Carácter Personal) a tal fin ha creado el siguiente Aviso Legal.</p>
+          <p>
+            <b>Qué hacen los diputados</b> es un proyecto de Political Watch que
+            a, su vez, forma parte de la Fundación Salvador Soler (en adelante
+            FSS).
+          </p>
+          <p>
+            FSS cumple con los requisitos Qué hacen los diputados la Ley
+            34/2002, de 11 de Julio, de Servicios de la Sociedad de la
+            Información y Comercio Electrónico, en la Ley 56/2007, de 28 de
+            diciembre de Medidas de Impulso de la Sociedad de la Información,
+            así como en la Ley Orgánica 15/1999, de 13 de diciembre, sobre
+            Protección de Datos de Carácter Personal y su normativa de
+            desarrollo (RD 1720/2007, de 21 de diciembre, por el que se aprueba
+            el Reglamento de Desarrollo de la Ley Orgánica 15/1999 de Protección
+            de Datos de Carácter Personal) a tal fin ha creado el siguiente
+            Aviso Legal.
+          </p>
           <pre>
               Fundación Salvador Soler
               Avenida Cardenal Herrera Oria 63, 3º Izq. 28034. Madrid.
@@ -19,38 +76,137 @@
               Datos del DPO: GRUPO ADAPTALIA – LEGAL FORMATIVO S.L.
               legal@grupoadaptalia.es 91 553 34 08
           </pre>
-          <p>El presente Aviso Legal regula el acceso y la utilización de la web, incluyendo los contenidos y los servicios puestos a disposición de los Usuarios a través de la web, bien por FSS, bien por sus Usuarios, bien por cualesquiera terceros.</p>
-          <p>El uso de la web se sujetará al presente Aviso Legal, el cuál podrá ser actualizado periódicamente, no obstante FSS avisará de la modificación del Aviso Legal o de la Política de Privacidad. Si el Usuario decide no aceptar el Aviso Legal deberá abstenerse de acceder y/o utilizar los contenidos y/o servicios ofrecidos en la web. FSS se reserva el derecho de mejorar, modificar o suprimir, unilateralmente y sin previo aviso, servicios y/o contenidos de dicha web atendiendo a los más diversos criterios, si bien informará de los mismos a los usuarios.</p>
-          <h3 id="obligaciones-de-hacer-un-uso-correcto-del-portal"><a href="#obligaciones-de-hacer-un-uso-correcto-del-portal" aria-hidden="true" tabindex="-1"><span class="icon icon-link"></span></a>Obligaciones de hacer un uso correcto del portal</h3>
-          <p>El Usuario se compromete a hacer un uso adecuado de la web y a no emplearla para incurrir en actividades ilícitas o contrarias a la buena fe y al ordenamiento legal (a título meramente indicativo y no exhaustivo); provocar daños en los sistemas físicos y lógicos del responsable, de sus proveedores o de terceras personas, introducir o difundir en la red virus informáticos o cualesquiera otros sistemas físicos o lógicos que sean susceptibles de provocar los daños anteriormente mencionados, sin olvidar todos aquellos que vulneren o transgredan el honor, la intimidad personal o familiar y a la imagen de las personas, de los derechos de propiedad y de toda otra naturaleza pertenecientes a un tercero como consecuencia de la transmisión, difusión, almacenamiento, puesta a disposición, recepción, obtención o acceso a los contenidos o la imagen de terceros; la realización de actos de competencia desleal y publicidad ilícita como consecuencia de la transmisión, difusión, almacenamiento, puesta a disposición, recepción, obtención o acceso a los contenidos; la falta de veracidad, exactitud, exhaustividad, pertinencia y/o actualidad de los contenidos dejando el Usuario, en todo caso, indemne a FSS frente a cualquier reclamación, judicial o extrajudicial.</p>
-          <h3 id="propiedad-intelectual-e-industrial"><a href="#propiedad-intelectual-e-industrial" aria-hidden="true" tabindex="-1"><span class="icon icon-link"></span></a>Propiedad intelectual e industrial</h3>
-          <p>Todas las marcas, nombres comerciales o cualquier otro signo distintivo son propiedad de FSS o de sus legítimos propietarios, quedando reservados todos los derechos sobre los mismos.</p>
-          <p>El contenido, así como la información objeto de propiedad intelectual cuya titularidad corresponda a FSS estará sujeta (salvo que no se especifique expresamente otra cosa en un contenido específico) a la licencia Creative Commons “By-SA 3.0” que aparece en la página principal.</p>
-          <h3 id="enlaces"><a href="#enlaces" aria-hidden="true" tabindex="-1"><span class="icon icon-link"></span></a>Enlaces</h3>
-          <p>FSS no asume ninguna responsabilidad sobre los enlaces externos que, en su caso, pudieran incluirse en el portal, ya que no tiene ningún tipo de control sobre los mismos, por lo que el Usuario accede bajo su exclusiva responsabilidad al contenido y en las condiciones de uso que rijan en los mismos.</p>
-          <h3 id="política-de-privacidad"><a href="#pol%C3%ADtica-de-privacidad" aria-hidden="true" tabindex="-1"><span class="icon icon-link"></span></a>Política de privacidad</h3>
-          <p>FSS protege los datos personales según lo establecido en la Ley Orgánica de Protección de Datos y el Reglamento de Medidas de Seguridad. La información sobre la protección de datos personales se encuentra recogida en la Política de Privacidad.</p>
-          <h3 id="duración-y-terminación"><a href="#duraci%C3%B3n-y-terminaci%C3%B3n" aria-hidden="true" tabindex="-1"><span class="icon icon-link"></span></a>Duración y terminación</h3>
-          <p>La web tiene, en principio, una duración indefinida. FSS, no obstante, está autorizada para dar por terminada o suspender la web en cualquier momento. Cuando ello sea razonablemente posible, FSS advertirá previamente sobre la terminación o suspensión de la Plataforma.</p>
-          <h3 id="jurisdicción-y-legislación-aplicable"><a href="#jurisdicci%C3%B3n-y-legislaci%C3%B3n-aplicable" aria-hidden="true" tabindex="-1"><span class="icon icon-link"></span></a>Jurisdicción y legislación aplicable</h3>
-          <p>Las presentes Condiciones se regirán por la legislación española. En caso de cualquier discrepancia las partes se someten a la jurisdicción de los Juzgados y Tribunales de Madrid con expresa renuncia a cualquier otro fuero que pudiera corresponderles, salvo que por imperativo legal corresponda otro distinto.</p>
+          <p>
+            El presente Aviso Legal regula el acceso y la utilización de la web,
+            incluyendo los contenidos y los servicios puestos a disposición de
+            los Usuarios a través de la web, bien por FSS, bien por sus
+            Usuarios, bien por cualesquiera terceros.
+          </p>
+          <p>
+            El uso de la web se sujetará al presente Aviso Legal, el cuál podrá
+            ser actualizado periódicamente, no obstante FSS avisará de la
+            modificación del Aviso Legal o de la Política de Privacidad. Si el
+            Usuario decide no aceptar el Aviso Legal deberá abstenerse de
+            acceder y/o utilizar los contenidos y/o servicios ofrecidos en la
+            web. FSS se reserva el derecho de mejorar, modificar o suprimir,
+            unilateralmente y sin previo aviso, servicios y/o contenidos de
+            dicha web atendiendo a los más diversos criterios, si bien informará
+            de los mismos a los usuarios.
+          </p>
+          <h3 id="obligaciones-de-hacer-un-uso-correcto-del-portal">
+            <a
+              href="#obligaciones-de-hacer-un-uso-correcto-del-portal"
+              aria-hidden="true"
+              tabindex="-1"
+              ><span class="icon icon-link"></span></a
+            >Obligaciones de hacer un uso correcto del portal
+          </h3>
+          <p>
+            El Usuario se compromete a hacer un uso adecuado de la web y a no
+            emplearla para incurrir en actividades ilícitas o contrarias a la
+            buena fe y al ordenamiento legal (a título meramente indicativo y no
+            exhaustivo); provocar daños en los sistemas físicos y lógicos del
+            responsable, de sus proveedores o de terceras personas, introducir o
+            difundir en la red virus informáticos o cualesquiera otros sistemas
+            físicos o lógicos que sean susceptibles de provocar los daños
+            anteriormente mencionados, sin olvidar todos aquellos que vulneren o
+            transgredan el honor, la intimidad personal o familiar y a la imagen
+            de las personas, de los derechos de propiedad y de toda otra
+            naturaleza pertenecientes a un tercero como consecuencia de la
+            transmisión, difusión, almacenamiento, puesta a disposición,
+            recepción, obtención o acceso a los contenidos o la imagen de
+            terceros; la realización de actos de competencia desleal y
+            publicidad ilícita como consecuencia de la transmisión, difusión,
+            almacenamiento, puesta a disposición, recepción, obtención o acceso
+            a los contenidos; la falta de veracidad, exactitud, exhaustividad,
+            pertinencia y/o actualidad de los contenidos dejando el Usuario, en
+            todo caso, indemne a FSS frente a cualquier reclamación, judicial o
+            extrajudicial.
+          </p>
+          <h3 id="propiedad-intelectual-e-industrial">
+            <a
+              href="#propiedad-intelectual-e-industrial"
+              aria-hidden="true"
+              tabindex="-1"
+              ><span class="icon icon-link"></span></a
+            >Propiedad intelectual e industrial
+          </h3>
+          <p>
+            Todas las marcas, nombres comerciales o cualquier otro signo
+            distintivo son propiedad de FSS o de sus legítimos propietarios,
+            quedando reservados todos los derechos sobre los mismos.
+          </p>
+          <p>
+            El contenido, así como la información objeto de propiedad
+            intelectual cuya titularidad corresponda a FSS estará sujeta (salvo
+            que no se especifique expresamente otra cosa en un contenido
+            específico) a la licencia Creative Commons “By-SA 3.0” que aparece
+            en la página principal.
+          </p>
+          <h3 id="enlaces">
+            <a href="#enlaces" aria-hidden="true" tabindex="-1"
+              ><span class="icon icon-link"></span></a
+            >Enlaces
+          </h3>
+          <p>
+            FSS no asume ninguna responsabilidad sobre los enlaces externos que,
+            en su caso, pudieran incluirse en el portal, ya que no tiene ningún
+            tipo de control sobre los mismos, por lo que el Usuario accede bajo
+            su exclusiva responsabilidad al contenido y en las condiciones de
+            uso que rijan en los mismos.
+          </p>
+          <h3 id="política-de-privacidad">
+            <a
+              href="#pol%C3%ADtica-de-privacidad"
+              aria-hidden="true"
+              tabindex="-1"
+              ><span class="icon icon-link"></span></a
+            >Política de privacidad
+          </h3>
+          <p>
+            FSS protege los datos personales según lo establecido en la Ley
+            Orgánica de Protección de Datos y el Reglamento de Medidas de
+            Seguridad. La información sobre la protección de datos personales se
+            encuentra recogida en la Política de Privacidad.
+          </p>
+          <h3 id="duración-y-terminación">
+            <a
+              href="#duraci%C3%B3n-y-terminaci%C3%B3n"
+              aria-hidden="true"
+              tabindex="-1"
+              ><span class="icon icon-link"></span></a
+            >Duración y terminación
+          </h3>
+          <p>
+            La web tiene, en principio, una duración indefinida. FSS, no
+            obstante, está autorizada para dar por terminada o suspender la web
+            en cualquier momento. Cuando ello sea razonablemente posible, FSS
+            advertirá previamente sobre la terminación o suspensión de la
+            Plataforma.
+          </p>
+          <h3 id="jurisdicción-y-legislación-aplicable">
+            <a
+              href="#jurisdicci%C3%B3n-y-legislaci%C3%B3n-aplicable"
+              aria-hidden="true"
+              tabindex="-1"
+              ><span class="icon icon-link"></span></a
+            >Jurisdicción y legislación aplicable
+          </h3>
+          <p>
+            Las presentes Condiciones se regirán por la legislación española. En
+            caso de cualquier discrepancia las partes se someten a la
+            jurisdicción de los Juzgados y Tribunales de Madrid con expresa
+            renuncia a cualquier otro fuero que pudiera corresponderles, salvo
+            que por imperativo legal corresponda otro distinto.
+          </p>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-
-  import PageHeader from '@/components/PageHeader.vue';
-
-  export default {
-      name: 'legal-notice',
-      components: {
-        PageHeader,
-      },
-    }
+<script setup>
+import PageHeader from "@/components/PageHeader.vue";
 </script>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
