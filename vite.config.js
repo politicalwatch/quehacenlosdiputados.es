3618b12b77317c7a7688703426e605f85259bd64
Alex Ahumada <alex.ahumada@politicalwatch.es>
2024-10-04 13:14:46 +0200

    Adds scss modern-compiler

--- vite.config.js
+++ vite.config.js
@@ -37,6 +37,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
+        api: "modern-compiler",
         additionalData: `@import "@/styles/App.scss";`,
       },
     },
