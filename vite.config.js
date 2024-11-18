acbe15ad2cd7f2f0d0598ec46ec6911db13628ac
Alex Ahumada <alex.ahumada@politicalwatch.es>
2024-11-18 12:02:45 +0100

    Silence sass import deprecation warnings

--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,7 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         api: "modern-compiler",
+        silenceDeprecations: ["import"],
         additionalData: `@import "@/styles/App.scss";`,
       },
     },
