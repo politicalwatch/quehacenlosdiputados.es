992e269d014e55187f0b47fcfde6f0201acf6af7
mike_gp <miguel.gomez.developer@gmail.com>
2025-02-24 13:01:32 +0100

    Actualizado footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -25,11 +25,6 @@
                   >Manifiesto</router-link
                 >
                 <router-link
-                  :to="{ name: 'topics' }"
-                  class="c-footer__menu-link"
-                  >Temáticas</router-link
-                >
-                <router-link
                   :to="{ name: 'footprint' }"
                   class="c-footer__menu-link"
                   >Índice parlamentario</router-link
@@ -73,18 +68,21 @@
               <h5 class="c-footer__section-title">Síguenos</h5>
               <div class="c-footer__section-icons">
                 <a
-                  href="https://twitter.com/qhld_"
-                  aria-label="Go to Twitter"
-                  target="_blank"
-                >
-                  <icon icon="mdi:twitter" class="c-icon--white" />
-                </a>
-                <a
                   href="https://www.instagram.com/_PoliticalWatch/"
                   aria-label="Go to Instagram"
                   target="_blank"
                   ><icon icon="mdi:instagram" class="c-icon--white"
                 /></a>
+                <a
+                  href="https://bsky.app/profile/politicalwatch.es"
+                  aria-label="Go to Bluesky"
+                  target="_blank"
+                >
+                  <!-- Replace with mdi when available -->
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="c-icon--white" width="1em" height="1em">
+                    <path d="M111.8 62.2C170.2 105.9 233 194.7 256 242.4c23-47.6 85.8-136.4 144.2-180.2c42.1-31.6 110.3-56 110.3 21.8c0 15.5-8.9 130.5-14.1 149.2C478.2 298 412 314.6 353.1 304.5c102.9 17.5 129.1 75.5 72.5 133.5c-107.4 110.2-154.3-27.6-166.3-62.9l0 0c-1.7-4.9-2.6-7.8-3.3-7.8s-1.6 3-3.3 7.8l0 0c-12 35.3-59 173.1-166.3 62.9c-56.5-58-30.4-116 72.5-133.5C100 314.6 33.8 298 15.7 233.1C10.4 214.4 1.5 99.4 1.5 83.9c0-77.8 68.2-53.4 110.3-21.8z"/>
+                  </svg>
+                </a>
                 <a
                   href="https://t.me/quehacenlosdiputados"
                   aria-label="Go to Telegram"
