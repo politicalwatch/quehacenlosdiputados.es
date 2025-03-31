aff960cc09c9997b2909a0c7d0718e66ba2dbe84
Alex Ahumada <alex.ahumada@politicalwatch.es>
2025-03-31 09:16:15 +0200

    Update vue-gtag to v3

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -54,9 +54,7 @@
                 <router-link :to="{ name: 'collaborate', hash: '#dona' }"
                   >Haz una donación</router-link
                 >
-                <router-link :to="{ name: 'contact' }"
-                  >Contacta</router-link
-                >
+                <router-link :to="{ name: 'contact' }">Contacta</router-link>
                 <a
                   href="mailto:qhld@politicalwatch.es?subject=Contacto+a+través+de+qhld.es"
                   target="_blank"
@@ -73,10 +71,18 @@
                   target="_blank"
                 >
                   <!-- Replace with mdi when available -->
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="c-icon--white" width="1em" height="1em">
-                    <path d="M111.8 62.2C170.2 105.9 233 194.7 256 242.4c23-47.6 85.8-136.4 144.2-180.2c42.1-31.6 110.3-56 110.3 21.8c0 15.5-8.9 130.5-14.1 149.2C478.2 298 412 314.6 353.1 304.5c102.9 17.5 129.1 75.5 72.5 133.5c-107.4 110.2-154.3-27.6-166.3-62.9l0 0c-1.7-4.9-2.6-7.8-3.3-7.8s-1.6 3-3.3 7.8l0 0c-12 35.3-59 173.1-166.3 62.9c-56.5-58-30.4-116 72.5-133.5C100 314.6 33.8 298 15.7 233.1C10.4 214.4 1.5 99.4 1.5 83.9c0-77.8 68.2-53.4 110.3-21.8z"/>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    class="c-icon--white"
+                    width="1em"
+                    height="1em"
+                  >
+                    <path
+                      d="M111.8 62.2C170.2 105.9 233 194.7 256 242.4c23-47.6 85.8-136.4 144.2-180.2c42.1-31.6 110.3-56 110.3 21.8c0 15.5-8.9 130.5-14.1 149.2C478.2 298 412 314.6 353.1 304.5c102.9 17.5 129.1 75.5 72.5 133.5c-107.4 110.2-154.3-27.6-166.3-62.9l0 0c-1.7-4.9-2.6-7.8-3.3-7.8s-1.6 3-3.3 7.8l0 0c-12 35.3-59 173.1-166.3 62.9c-56.5-58-30.4-116 72.5-133.5C100 314.6 33.8 298 15.7 233.1C10.4 214.4 1.5 99.4 1.5 83.9c0-77.8 68.2-53.4 110.3-21.8z"
+                    />
                   </svg>
-                </a>                              
+                </a>
                 <a
                   href="https://www.instagram.com/_PoliticalWatch/"
                   aria-label="Go to Instagram"
@@ -156,7 +162,7 @@
 import { defineAsyncComponent } from "vue";
 import VueCookieAcceptDecline from "vue-cookie-accept-decline";
 import "vue-cookie-accept-decline/dist/vue-cookie-accept-decline.css";
-import { bootstrap } from "vue-gtag";
+import { addGtag, consent, consentDeniedAll } from "vue-gtag";
 import { Icon } from "@iconify/vue";
 
 import LogoPW from "@/assets/logo-political-watch.svg";
@@ -173,15 +179,13 @@ const DynamicLogo = defineAsyncComponent(
 const cookieStatus = (val) => {
   // console.log('Cookie status: ' + val);
   if (val === "decline" || val == null) {
-    if (gtag) {
-      gtag("consent", "default", {
-        ad_storage: "denied",
-        analytics_storage: "denied",
-      });
-    }
+    consent("default", {
+      ad_storage: "denied",
+      analytics_storage: "denied",
+    });
   } else if (val === "accept") {
-    bootstrap().then(() => {
-      gtag("consent", "update", {
+    addGtag().then(() => {
+      consent("update", {
         ad_storage: "granted",
         analytics_storage: "granted",
       });
@@ -190,8 +194,8 @@ const cookieStatus = (val) => {
 };
 
 const cookieClickedAccept = () => {
-  bootstrap().then(() => {
-    gtag("consent", "update", {
+  addGtag().then(() => {
+    consent("update", {
       ad_storage: "granted",
       analytics_storage: "granted",
     });
@@ -199,7 +203,7 @@ const cookieClickedAccept = () => {
 };
 
 const cookieClickedDecline = () => {
-  gtag("consent", "default", {
+  consent("default", {
     ad_storage: "denied",
     analytics_storage: "denied",
   });
--- src/main.js
+++ src/main.js
@@ -3,23 +3,22 @@ import App from "./App.vue";
 import { createPinia } from "pinia";
 import { createHead } from "@unhead/vue/client";
 import router from "@/router";
-import VueGtag from "vue-gtag";
+import { createGtag } from "vue-gtag";
 import VueScrollTo from "vue-scrollto";
-
-// import "./styles/App.scss";
 
 const app = createApp(App);
 app.use(router);
 app.use(createPinia());
 app.use(createHead());
-app.use(
-  VueGtag,
-  {
-    config: { id: import.meta.env.VITE_GA_ID },
-    boootstrap: false,
+
+const gtag = createGtag({
+  tagId: import.meta.env.VITE_GA_ID,
+  pageTracker: {
+    router,
   },
-  router
-);
+  initMode: "manual",
+});
+app.use(gtag);
 
 app.directive("scroll-to", VueScrollTo);
 
